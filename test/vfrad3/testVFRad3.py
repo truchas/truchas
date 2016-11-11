c33b08c47ce61a7bffb08430b281b53ef283dd7b
Neil Carlson <neil.n.carlson@gmail.com>
2016-11-11 13:49:09 -0700

    Add view factor radiation test problem vfrad3

---
+++ test/vfrad3/testVFRad3.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+import numpy
+
+import Truchas
+import TruchasTest
+
+class VFRad3(TruchasTest.GoldenTestCase):
+
+  test_name = 'vfrad3'
+  num_procs = 4 # with a parallel executable
+  
+  def test_final_cycle_number(self):
+    '''VFRAD3: checking the final cycle number'''
+    test_series = self.test_output.get_simulation().find_series(id=2)
+    gold_series = self.gold_output.get_simulation().find_series(id=2)
+    self.assertTrue(test_series.cycle == gold_series.cycle)
+
+  def test_final_temperature(self):
+    '''VFRAD3: verifying the final temperature field'''
+    tol = 1.0e-8
+    test = self.test_output.get_simulation().find_series(id=2).get_data('Z_TEMP')
+    gold = self.gold_output.get_simulation().find_series(id=2).get_data('Z_TEMP')
+    error = max(abs(test-gold)/gold)
+    print 'final temp rel error=', error, '(tol=', tol, ')'
+    self.assertTrue(error <= tol)
+
+if __name__ == '__main__':
+  import unittest
+  unittest.main()
+
