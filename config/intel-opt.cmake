d8f4ca47973b6e94ea7a9008130d47a3517ed571
Neil Carlson <neil.n.carlson@gmail.com>
2017-10-18 11:49:51 -0600

    Add -fpe0 compiler option to Intel cmake files (see issue #150)

--- config/intel-opt.cmake
+++ config/intel-opt.cmake
@@ -6,4 +6,4 @@ set(CMAKE_C_COMPILER icc CACHE STRING "C Compiler")
 set(CMAKE_Fortran_COMPILER ifort CACHE STRING "Fortran Compiler")
 
 # Additional flags to the default CMAKE_<lang>_FLAGS_<build_type> flags
-set(CMAKE_Fortran_FLAGS "-u" CACHE STRING "Fortran compile flags")
+set(CMAKE_Fortran_FLAGS "-u -traceback -fpe0" CACHE STRING "Fortran compile flags")
