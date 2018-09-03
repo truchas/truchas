b9bb70129b23a9ba98e471f6f4d1e36d258625f7
Neil Carlson <neil.n.carlson@gmail.com>
2018-09-03 10:47:06 -0600

    Add pipe flow tests for old and new flow solver

---
+++ test/pipe-flow-old/test4.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+import numpy
+import math
+
+import Truchas
+import TruchasTest
+
+class mytest(TruchasTest.GoldenTestCase):
+
+  test_name = 'pipe-flow-old-inviscid-4'
+  num_procs = 4 # with a parallel executable
+
+  # Override the default setUp, omitting the opening of the golden output
+  def setUp(self):
+    if self._is_initialized is False:
+      self.setUpClass() # This runs Truchas
+    self.test_output = Truchas.TruchasOutput(self.get_output_file())
+
+  def test_early_velocity(self):
+    '''PIPE-FLOW-INVISCID-4: early velocity'''
+
+    time = self.test_output.get_simulation().find_series(id=2).time
+    data = self.test_output.get_simulation().find_series(id=2).get_data('Z_VC')
+    print 'velocity at t=%11.5e'%(time)
+
+    fail = 0
+
+    tol = 1.0e-11
+    vel = math.sqrt(2)*time
+    error = max(abs((data[:,0] - vel)/vel))
+    if (error > tol):
+      fail += 1
+      print 'x-velocity: max rel error = %8.2e: FAIL (tol=%8.2e)'%(error,tol)
+    else:
+      print 'x-velocity: max rel error = %8.2e: PASS (tol=%8.2e)'%(error,tol)
+
+    error = max(abs((data[:,1] - vel)/vel))
+    if (error > tol):
+      fail += 1
+      print 'y-velocity: max rel error = %8.2e: FAIL (tol=%8.2e)'%(error,tol)
+    else:
+      print 'y-velocity: max rel error = %8.2e: PASS (tol=%8.2e)'%(error,tol)
+
+    tol = 1.0e-12
+    error = max(abs(data[:,2]))
+    if (error > tol):
+      fail += 1
+      print 'z-velocity: max error = %8.2e: FAIL (tol=%8.2e)'%(error,tol)
+    else:
+      print 'z-velocity: max error = %8.2e: PASS (tol=%8.2e)'%(error,tol)
+
+    self.assertTrue(fail == 0)
+
+  def test_final_velocity(self):
+    '''PIPE-FLOW-INVISCID-4: final velocity'''
+
+    time = self.test_output.get_simulation().find_series(id=3).time
+    data = self.test_output.get_simulation().find_series(id=3).get_data('Z_VC')
+    print 'velocity at t=%11.5e'%(time)
+
+    fail = 0
+
+    tol = 1.0e-12
+    vel = math.sqrt(2)*time
+    error = max(abs(data[:,0] - vel))
+    if (error > tol):
+      fail += 1
+      print 'x-velocity: max error = %8.2e: FAIL (tol=%8.2e)'%(error,tol)
+    else:
+      print 'x-velocity: max error = %8.2e: PASS (tol=%8.2e)'%(error,tol)
+
+    error = max(abs(data[:,1] - vel))
+    if (error > tol):
+      fail += 1
+      print 'y-velocity: max error = %8.2e: FAIL (tol=%8.2e)'%(error,tol)
+    else:
+      print 'y-velocity: max error = %8.2e: PASS (tol=%8.2e)'%(error,tol)
+
+    tol = 1.0e-13
+    error = max(abs(data[:,2]))
+    if (error > tol):
+      fail += 1
+      print 'z-velocity: max error = %8.2e: FAIL (tol=%8.2e)'%(error,tol)
+    else:
+      print 'z-velocity: max error = %8.2e: PASS (tol=%8.2e)'%(error,tol)
+
+    self.assertTrue(fail == 0)
+
+if __name__ == '__main__':
+  import unittest
+  unittest.main()
+
---
+++ test/pipe-flow/test2.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+import numpy
+
+import Truchas
+import TruchasTest
+
+class mytest(TruchasTest.GoldenTestCase):
+
+  test_name = 'pipe-flow-inviscid-2'
+  num_procs = 4 # with a parallel executable
+
+  # Override the default setUp, omitting the opening of the golden output
+  def setUp(self):
+    if self._is_initialized is False:
+      self.setUpClass() # This runs Truchas
+    self.test_output = Truchas.TruchasOutput(self.get_output_file())
+
+  def test_early_velocity(self):
+    '''PIPE-FLOW-INVISCID-2: early velocity'''
+
+    time = self.test_output.get_simulation().find_series(id=2).time
+    data = self.test_output.get_simulation().find_series(id=2).get_data('Z_VC')
+    print 'velocity at t=%11.5e'%(time)
+
+    fail = 0
+
+    tol = 1.0e-11
+    error = max(abs((data[:,0] - 2*time)/(2*time)))
+    if (error > tol):
+      fail += 1
+      print 'x-velocity: max rel error = %8.2e: FAIL (tol=%8.2e)'%(error,tol)
+    else:
+      print 'x-velocity: max rel error = %8.2e: PASS (tol=%8.2e)'%(error,tol)
+
+    tol = 1.0e-12
+    error = max(abs(data[:,1]))
+    if (error > tol):
+      fail += 1
+      print 'y-velocity: max error = %8.2e: FAIL (tol=%8.2e)'%(error,tol)
+    else:
+      print 'y-velocity: max error = %8.2e: PASS (tol=%8.2e)'%(error,tol)
+
+    error = max(abs(data[:,2]))
+    if (error > tol):
+      fail += 1
+      print 'z-velocity: max error = %8.2e: FAIL (tol=%8.2e)'%(error,tol)
+    else:
+      print 'z-velocity: max error = %8.2e: PASS (tol=%8.2e)'%(error,tol)
+
+    self.assertTrue(fail == 0)
+
+  def test_final_velocity(self):
+    '''PIPE-FLOW-INVISCID-2: final velocity'''
+
+    time = self.test_output.get_simulation().find_series(id=3).time
+    data = self.test_output.get_simulation().find_series(id=3).get_data('Z_VC')
+    print 'velocity at t=%11.5e'%(time)
+
+    fail = 0
+
+    tol = 1.0e-12
+    error = max(abs(data[:,0] - 2*time))
+    if (error > tol):
+      fail += 1
+      print 'x-velocity: max error = %8.2e: FAIL (tol=%8.2e)'%(error,tol)
+    else:
+      print 'x-velocity: max error = %8.2e: PASS (tol=%8.2e)'%(error,tol)
+
+    tol = 1.0e-13
+    error = max(abs(data[:,1]))
+    if (error > tol):
+      fail += 1
+      print 'y-velocity: max error = %8.2e: FAIL (tol=%8.2e)'%(error,tol)
+    else:
+      print 'y-velocity: max error = %8.2e: PASS (tol=%8.2e)'%(error,tol)
+
+    error = max(abs(data[:,2]))
+    if (error > tol):
+      fail += 1
+      print 'z-velocity: max error = %8.2e: FAIL (tol=%8.2e)'%(error,tol)
+    else:
+      print 'z-velocity: max error = %8.2e: PASS (tol=%8.2e)'%(error,tol)
+
+    self.assertTrue(fail == 0)
+
+if __name__ == '__main__':
+  import unittest
+  unittest.main()
+
