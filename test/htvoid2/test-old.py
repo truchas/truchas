9946bc7fd9a4b2b2827d22441e854522472023aa
Neil Carlson <neil.n.carlson@gmail.com>
2018-11-22 10:09:30 -0700

    Migrate htvoid? tests to htvoid?-old

--- test/htvoid2/test-old.py
+++ test/htvoid2/test-old.py
@@ -8,19 +8,19 @@ import numpy
 import Truchas
 import TruchasTest
 
-class HTVoid1(TruchasTest.GoldenTestCase):
+class HTVoid2(TruchasTest.GoldenTestCase):
 
-  test_name = 'htvoid1'
+  test_name = 'htvoid2-old'
   num_procs = 4 # with a parallel executable
 
   def test_final_cycle_number(self):
-    '''HTVOID1: checking the final cycle number'''
+    '''HTVOID2: checking the final cycle number'''
     test_series = self.test_output.get_simulation().find_series(id=2)
     gold_series = self.gold_output.get_simulation().find_series(id=2)
     self.assertTrue(test_series.cycle == gold_series.cycle)
-  
+
   def test_final_temp(self):
-    '''HTVOID1: verifying the temperature field at final time'''
+    '''HTVOID2: verifying the temperature field at final time'''
     tol = 1.0e-6
     test = self.test_output.get_simulation().find_series(id=2).get_data('Z_TEMP')
     gold = self.gold_output.get_simulation().find_series(id=2).get_data('Z_TEMP')
--- test/htvoid4/test-old.py
+++ test/htvoid4/test-old.py
@@ -8,32 +8,32 @@ import numpy
 import Truchas
 import TruchasTest
 
-class HTVoid3(TruchasTest.GoldenTestCase):
+class HTVoid4(TruchasTest.GoldenTestCase):
 
-  test_name = 'htvoid3'
+  test_name = 'htvoid4-old'
   num_procs = 4 # with a parallel executable
 
   def test_final_cycle_number(self):
-    '''HTVOID3: checking the final cycle number'''
+    '''HTVOID4: checking the final cycle number'''
     test_series = self.test_output.get_simulation().find_series(id=2)
     gold_series = self.gold_output.get_simulation().find_series(id=2)
     self.assertTrue(test_series.cycle == gold_series.cycle)
 
   def test_final_temp(self):
-    '''HTVOID3: verifying the temperature field at final time'''
-    tol = 3.0e-4
+    '''HTVOID4: verifying the temperature field at final time'''
+    tol = 1.0e-6
     test = self.test_output.get_simulation().find_series(id=2).get_data('Z_TEMP')
     gold = self.gold_output.get_simulation().find_series(id=2).get_data('Z_TEMP')
     error = max(abs(test-gold))
     print 'final temp max error=', error, '(tol=', tol, ')'
     self.assertTrue(error <= tol)
 
-  def test_final_solid_frac(self):
-    '''HTVOID3: verifying the solid volume fraction at final time'''
-    tol = 2.0e-4
+  def test_final_fluid_frac(self):
+    '''HTVOID4: verifying the fluid volume fraction at final time'''
+    tol = 1.0e-6
     test = self.test_output.get_simulation().find_series(id=2).get_data('VOF')
     gold = self.gold_output.get_simulation().find_series(id=2).get_data('VOF')
-    error = max(abs(test[:,1]-gold[:,1])) # comp 1 is solid
+    error = max(abs(test[:,1]-gold[:,1])) # comp 1 is fluid
     print 'final vof max error=', error, '(tol=', tol, ')'
     self.assertTrue(error <= tol)
 
