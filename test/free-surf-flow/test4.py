3a0d2341ea061e63796b72eacb93b4d628dc868c
Neil Carlson <neil.n.carlson@gmail.com>
2019-01-10 15:07:23 -0700

    Tighten tolerances on free-surf-flow-4; test for #266

--- test/free-surf-flow/test4.py
+++ test/free-surf-flow/test4.py
@@ -18,12 +18,12 @@ class mytest(TruchasTest.GoldenTestCase):
     
     # Initial conditions
     success &= self.vof_test(1, 0.0)
-    success &= self.pressure_test(1, 1e-8)
+    success &= self.pressure_test(1, 1e-14)
     
     # Final time
-    success &= self.vof_test(2, 1e-10)
-    success &= self.pressure_test(2, 1e-10)
-    success &= self.velocity_test(2, 1e-10)
+    success &= self.vof_test(2, 1e-13)
+    success &= self.pressure_test(2, 1e-14)
+    success &= self.velocity_test(2, 1e-14)
     
     self.assertTrue(success)
 
