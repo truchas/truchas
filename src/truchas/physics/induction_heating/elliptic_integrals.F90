f3a87aa485fa2ea06066157f34d90fa83fe1fc87
Neil Carlson <neil.n.carlson@gmail.com>
2019-03-01 08:11:50 -0700

    Update copyright, LANS => Triad

--- src/truchas/physics/induction_heating/elliptic_integrals.F90
+++ src/truchas/physics/induction_heating/elliptic_integrals.F90
@@ -12,9 +12,7 @@
 !!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!
-!! Copyright (c) Los Alamos National Security, LLC.  This file is part of the
-!! Truchas code (LA-CC-15-097) and is subject to the revised BSD license terms
-!! in the LICENSE file found in the top-level directory of this distribution.
+!! This file is part of Truchas. 3-Clause BSD license; see the LICENSE file.
 !!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!
