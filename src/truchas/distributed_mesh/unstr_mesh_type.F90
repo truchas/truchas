b2e3ff61d0006adbde0b2db2ead6f4f6630a49e5
Neil Carlson <neil.n.carlson@gmail.com>
2019-06-03 16:31:47 -0600

    Add nearest_cell/nearest_node methods to unstr_mesh type (ref #282)

--- src/truchas/distributed_mesh/unstr_mesh_type.F90
+++ src/truchas/distributed_mesh/unstr_mesh_type.F90
@@ -169,6 +169,8 @@ module unstr_mesh_type
     procedure :: init_cell_centroid
     procedure :: init_face_centroid
     procedure :: init_face_normal_dist
+    procedure :: nearest_node
+    procedure :: nearest_cell
   end type unstr_mesh
 
 contains
@@ -507,5 +509,80 @@ contains
     setids = pack(this%link_set_id, mask=btest(bitmask, pos=[(j,j=1,size(this%link_set_id))]))
 
   end subroutine get_link_set_ids
+
+  !! This function identifies the global cell that is nearest the given point,
+  !! and returns its local index on the process that owns the cell, and 0 on
+  !! all other processes. Here nearest means the cell whose centroid is nearest
+  !! the given point. Least cell index breaks ties. Note that this means the
+  !! cell may not actually contain the point at all. The immediate purpose is
+  !! in solution probe initialization, and it may not be suitable for anything
+  !! else.
+
+  function nearest_cell(this, point)
+
+    class(unstr_mesh), intent(in) :: this
+    real(r8), intent(in) :: point(:)
+    integer :: nearest_cell
+
+    integer :: j, min_cell, min_PE
+    real(r8) :: min_dist, d, array(nPE), centroid(size(this%x,dim=1))
+
+    ASSERT(size(point) == size(this%x,dim=1))
+
+    !! Compute the minimum distance and cell index for the local mesh subdomain
+    min_dist = huge(min_dist)
+    do j = 1, this%ncell_onP
+      associate(cell_nodes => this%cnode(this%xcnode(j):this%xcnode(j+1)-1))
+        centroid = sum(this%x(:,cell_nodes),dim=2)/size(cell_nodes)
+      end associate
+      d = norm2(centroid-point)
+      if (d < min_dist) then
+        min_dist = d
+        min_cell = j
+      end if
+    end do
+
+    !! Determine the nearest cell and its owner globally
+    call collate(array, min_dist)
+    if (is_IOP) min_PE = minloc(array,dim=1)
+    call broadcast(min_PE)
+    nearest_cell = merge(min_cell, 0, (this_PE == min_PE))
+
+  end function nearest_cell
+
+  !! This function identifies the global node that is nearest the given point,
+  !! and returns its local index on the process that owns the node, and 0 on
+  !! all other processes. Least node index breaks ties.  The immediate purpose
+  !! is in solution probe initialization, and it may not be suitable for
+  !! anything else.
+
+  function nearest_node(this, point)
+
+    class(unstr_mesh), intent(in) :: this
+    real(r8), intent(in) :: point(:)
+    integer :: nearest_node
+
+    integer :: j, min_node, min_PE
+    real(r8) :: min_dist, d, array(nPE)
+
+    ASSERT(size(point) == size(this%x,dim=1))
+
+    !! Compute the minimum distance and node index for the local mesh subdomain.
+    min_dist = huge(min_dist)
+    do j = 1, this%nnode_onP
+      d = norm2(this%x(:,j)-point)
+      if (d < min_dist) then
+        min_dist = d
+        min_node = j
+      end if
+    end do
+
+    !! Determine the nearest node and its owner globally.
+    call collate(array, min_dist)
+    if (is_IOP) min_PE = minloc(array,dim=1)
+    call broadcast(min_PE)
+    nearest_node = merge(min_node, 0, (this_PE == min_PE))
+
+  end function nearest_node
 
 end module unstr_mesh_type
