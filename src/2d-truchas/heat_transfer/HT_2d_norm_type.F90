e2474798c2ad638ba5f0c9338a352e154985cbf5
David Neill Asanza <dhna@lanl.gov>
2020-05-26 17:42:25 -0600

    Implemented 2D HT error norm.

---
+++ src/2d-truchas/heat_transfer/HT_2d_norm_type.F90
@@ -0,0 +1,113 @@
+!TODO: finish documentation
+!! HT_2D_NORM_TYPE
+!!
+!! David Neill-Asanza <dhna@lanl.gov>
+!! May 2020
+!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!
+!! This file is part of Truchas. 3-Clause BSD license; see the LICENSE file.
+!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+#include "f90_assert.fpp"
+
+module HT_2d_norm_type
+
+  use kinds
+  use HT_2d_model_type
+  use parameter_list_type
+  use truchas_logging_services
+  implicit none
+  private
+
+  type, public:: HT_2d_norm
+    private
+    ! type(unstr_2d_mesh), pointer :: mesh => null()  ! reference only -- do not own
+    type(HT_2d_model), pointer :: model => null()   ! reference only -- do not own
+    real(r8) :: abs_T_tol   ! absolute temperature tolerance
+    real(r8) :: rel_T_tol   ! relative temperature tolerance
+    real(r8) :: abs_H_tol   ! absolute enthalpy tolerance
+    real(r8) :: rel_H_tol   ! relative enthalpy tolerance
+  contains
+    procedure :: init
+    procedure :: compute
+  end type HT_2d_norm
+
+contains
+
+  subroutine init(this, model, params)
+
+    class(HT_2d_norm), intent(out) :: this
+    type(HT_2d_model), intent(in), target :: model
+    type(parameter_list) :: params
+
+    integer :: stat
+    character(:), allocatable :: context, errmsg
+
+    this%model => model
+
+    context = 'processing ' // params%name() // ': '
+    call params%get('temp-abs-tol', this%abs_T_tol, default=0.0_r8, stat=stat, errmsg=errmsg)
+    if (stat /= 0) call TLS_fatal(context//errmsg)
+    call params%get('temp-rel-tol', this%rel_T_tol, default=0.0_r8, stat=stat, errmsg=errmsg)
+    if (stat /= 0) call TLS_fatal(context//errmsg)
+    if (this%abs_T_tol < 0.0_r8) call TLS_fatal(context//'"temp-abs-tol" must be >= 0.0')
+    if (this%rel_T_tol < 0.0_r8) call TLS_fatal(context//'"temp-rel-tol" must be >= 0.0')
+    if (this%abs_T_tol == 0.0_r8 .and. this%rel_T_tol == 0.0_r8) &
+        call TLS_fatal(context//'"temp-abs-tol" and "temp-rel-tol" cannot both be 0.0')
+
+    call params%get('enth-abs-tol', this%abs_H_tol, default=0.0_r8, stat=stat, errmsg=errmsg)
+    if (stat /= 0) call TLS_fatal(context//errmsg)
+    call params%get('enth-rel-tol', this%rel_H_tol, default=0.0_r8, stat=stat, errmsg=errmsg)
+    if (stat /= 0) call TLS_fatal(context//errmsg)
+    if (this%abs_H_tol < 0.0_r8) call TLS_fatal(context//'"enth-abs-tol" must be >= 0.0')
+    if (this%rel_H_tol < 0.0_r8) call TLS_fatal(context//'"enth-rel-tol" must be >= 0.0')
+    if (this%abs_H_tol == 0.0_r8 .and. this%rel_H_tol == 0.0_r8) &
+        call TLS_fatal(context//'"enth-abs-tol" and "enth-rel-tol" cannot both be 0.0')
+
+  end subroutine init
+
+
+  subroutine compute(this, u, du, du_norm)
+
+    use parallel_communication, only: global_maxval
+
+    class(HT_2d_norm), intent(in) :: this
+    real(r8), intent(in), target :: u(:), du(:)
+    real(r8), intent(out) :: du_norm
+
+    real(r8), pointer :: useg(:), duseg(:)
+
+    ASSERT(size(u) == size(du))
+    ASSERT(size(u) == this%model%num_dof())
+
+    du_norm = 0.0_r8
+
+    !! Cell temperature delta norm
+    call this%model%get_cell_temp_view(u, useg)
+    call this%model%get_cell_temp_view(du, duseg)
+    du_norm = max(du_norm, maxerr(useg, duseg, this%abs_T_tol, this%rel_T_tol))
+
+    !! Face temperature delta norm
+    call this%model%get_face_temp_view(u, useg)
+    call this%model%get_face_temp_view(du, duseg)
+    du_norm = max(du_norm, maxerr(useg, duseg, this%abs_T_tol, this%rel_T_tol))
+
+    !! Cell enthalpy delta norm
+    call this%model%get_cell_heat_view(u, useg)
+    call this%model%get_cell_heat_view(du, duseg)
+    du_norm = max(du_norm, maxerr(useg, duseg, this%abs_H_tol, this%rel_H_tol))
+
+    du_norm = global_maxval(du_norm)
+
+  contains
+
+    real(r8) function maxerr(u, du, atol, rtol)
+      real(r8), intent(in) :: u(:), du(:), atol, rtol
+      maxerr = maxval(abs(du)/(atol + rtol*abs(u)))
+    end function
+
+  end subroutine compute
+
+end module HT_2d_norm_type
---
+++ src/2d-truchas/heat_transfer/Test/test_HT_2d_norm_type.F90
@@ -0,0 +1,309 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!
+!! This file is part of Truchas. 3-Clause BSD license; see the LICENSE file.
+!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+program test_HT_2d_norm_type
+
+#ifdef NAGFOR
+  use,intrinsic :: f90_unix, only: exit
+#endif
+  use kinds, only: r8
+  use pgslib_module, only: PGSLib_CL_MAX_TOKEN_LENGTH
+  use parallel_util_module, only: parallel_init
+  use parallel_communication
+  use truchas_env, only: prefix
+  use truchas_logging_services
+  use parameter_list_type
+  use parameter_list_json
+  use unstr_2d_mesh_factory
+  use matl_mesh_func_type
+  use material_database_type
+  use source_mesh_function
+  use scalar_func_factories
+  use mfd_2d_disc_type
+  use HT_2d_model_type
+  use HT_2d_norm_type
+  use bitfield_type
+  use test_ht_2d_common
+  implicit none
+
+  character(PGSLib_CL_MAX_TOKEN_LENGTH), pointer :: argv(:) => null()
+
+  type(unstr_2d_mesh), pointer :: mesh
+  type(mfd_2d_disc), target :: mfd_disc
+  type(material_database), target :: matl_db
+  type(matl_mesh_func), target :: mmf
+  real(r8) :: xmin(2), xmax(2), eps
+  integer  :: nx(2)
+  integer :: status = 0
+
+  !! Initialize MPI and other base stuff that Truchas depends on
+  call parallel_init(argv)
+  call init_parallel_communication
+  prefix='run'  ! TLS will write to 'run.log'
+  call TLS_initialize
+  call TLS_set_verbosity(TLS_VERB_NORMAL)
+
+  eps = 0.0_r8  ! mesh distortion
+  xmin = [0.0_r8, 0.0_r8]
+  xmax = [1.0_r8, 1.0_r8]
+  nx  = [64, 64]
+
+  !! Create the mesh specified by the above input file
+  mesh => new_unstr_2d_mesh(xmin, xmax, nx, eps)
+
+  !! Initialize state needed by all tests
+  call mfd_disc%init(mesh)
+  call init_materials(mesh, matl_db, mmf)
+
+  !! Run test problems
+  call test_abs(mfd_disc, mmf)
+  call test_rel(mfd_disc, mmf)
+  call test_mixed(mfd_disc, mmf)
+  call test_global(mfd_disc, mmf)
+
+  !! Wrap up
+  call halt_parallel_communication
+  call exit (status)
+
+contains
+
+  subroutine error_exit(errmsg)
+    character(len=*), intent(in) :: errmsg
+    status = 1
+    if (is_IOP) print '("FATAL: ",a)', errmsg
+    call halt_parallel_communication
+    call exit(status)
+  end subroutine error_exit
+
+
+  !! Test the HT_2d_norm type with only the absolute tolerance set
+  subroutine test_abs(disc, mmf)
+
+    type(mfd_2d_disc), target, intent(in) :: disc
+    type(matl_mesh_func), target, intent(in) :: mmf
+
+    type(HT_2d_model), target :: HT_model
+    type(HT_2d_norm), target :: HT_norm
+    type(parameter_list), pointer :: params, sublist
+    real(r8), allocatable :: u(:), du(:)
+    character(:), allocatable :: errmsg, string
+    real(r8) :: du_norm
+    integer :: n, stat
+
+    if (is_IOP) print '(/,"Testing absolute tolerance")'
+
+    !! Define problem parameters
+    string = &
+    '{"model": { &
+        "bc": { &
+          "all-sides": { &
+            "type": "temperature", &
+            "face-set-ids": [1,2,3,4], &
+            "temp": 0.0}}}, &
+      "error-norm": { &
+        "temp-abs-tol": 0.5, &
+        "enth-abs-tol": 0.5}}'
+    call parameter_list_from_json_string(string, params, errmsg)
+    if (.not. associated(params)) call error_exit(errmsg)
+
+    !! Initialize 2D HT model
+    sublist => params%sublist('model')
+    call HT_model%init(disc, mmf, sublist, stat, errmsg)
+    if (stat /= 0) call error_exit(errmsg)
+
+    !! Initialize 2D HT norm
+    sublist => params%sublist('error-norm')
+    call HT_norm%init(HT_model, sublist)
+
+    !! Compute norm
+    n = HT_model%num_dof()
+    allocate(u(n), du(n))
+    u = -10.0_r8
+    du = -0.5_r8
+    call HT_norm%compute(u, du, du_norm)
+
+    !! Check result
+    if (global_maxval(abs(du_norm-1.0_r8)) /= 0.0_r8) then
+      if (is_IOP) print '("ERROR: norm is nonzero")'
+      status = 1
+    end if
+
+  end subroutine test_abs
+
+
+  !! Test the HT_2d_norm type with only the relative tolerance set
+  subroutine test_rel(disc, mmf)
+
+    type(mfd_2d_disc), target, intent(in) :: disc
+    type(matl_mesh_func), target, intent(in) :: mmf
+
+    type(HT_2d_model), target :: HT_model
+    type(HT_2d_norm), target :: HT_norm
+    type(parameter_list), pointer :: params, sublist
+    real(r8), allocatable :: u(:), du(:)
+    character(:), allocatable :: errmsg, string
+    real(r8) :: du_norm
+    integer :: n, stat
+
+    if (is_IOP) print '(/,"Testing relative tolerance")'
+
+    !! Define problem parameters
+    string = &
+    '{"model": { &
+        "bc": { &
+          "all-sides": { &
+            "type": "temperature", &
+            "face-set-ids": [1,2,3,4], &
+            "temp": 0.0}}}, &
+      "error-norm": { &
+        "temp-rel-tol": 0.25, &
+        "enth-rel-tol": 0.25}}'
+    call parameter_list_from_json_string(string, params, errmsg)
+    if (.not. associated(params)) call error_exit(errmsg)
+
+    !! Initialize 2D HT model
+    sublist => params%sublist('model')
+    call HT_model%init(disc, mmf, sublist, stat, errmsg)
+    if (stat /= 0) call error_exit(errmsg)
+
+    !! Initialize 2D HT norm
+    sublist => params%sublist('error-norm')
+    call HT_norm%init(HT_model, sublist)
+
+    !! Compute norm
+    n = HT_model%num_dof()
+    allocate(u(n), du(n))
+    u = -2.0_r8
+    du = -1.0_r8
+    call HT_norm%compute(u, du, du_norm)
+
+    !! Check result
+    if (global_maxval(abs(du_norm-2.0_r8)) /= 0.0_r8) then
+      if (is_IOP) print '("ERROR: norm is nonzero")'
+      status = 1
+    end if
+
+  end subroutine test_rel
+
+
+  !! Test the HT_2d_norm type with mixed tolerances
+  subroutine test_mixed(disc, mmf)
+
+    type(mfd_2d_disc), target, intent(in) :: disc
+    type(matl_mesh_func), target, intent(in) :: mmf
+
+    type(HT_2d_model), target :: HT_model
+    type(HT_2d_norm), target :: HT_norm
+    type(parameter_list), pointer :: params, sublist
+    real(r8), allocatable :: u(:), du(:)
+    character(:), allocatable :: errmsg, string
+    real(r8) :: du_norm
+    integer :: n, stat
+
+    if (is_IOP) print '(/,"Testing mixed tolerances")'
+
+    !! Define problem parameters
+    string = &
+    '{"model": { &
+        "bc": { &
+          "all-sides": { &
+            "type": "temperature", &
+            "face-set-ids": [1,2,3,4], &
+            "temp": 0.0}}}, &
+      "error-norm": { &
+        "temp-abs-tol": 1.0, &
+        "temp-rel-tol": 2.0, &
+        "enth-abs-tol": 1.0, &
+        "enth-rel-tol": 2.0}}'
+    call parameter_list_from_json_string(string, params, errmsg)
+    if (.not. associated(params)) call error_exit(errmsg)
+
+    !! Initialize 2D HT model
+    sublist => params%sublist('model')
+    call HT_model%init(disc, mmf, sublist, stat, errmsg)
+    if (stat /= 0) call error_exit(errmsg)
+
+    !! Initialize 2D HT norm
+    sublist => params%sublist('error-norm')
+    call HT_norm%init(HT_model, sublist)
+
+    !! Compute norm
+    n = HT_model%num_dof()
+    allocate(u(n), du(n))
+    u = -1.5_r8
+    du = -4.0_r8
+    call HT_norm%compute(u, du, du_norm)
+
+    !! Check result
+    if (global_maxval(abs(du_norm-1.0_r8)) /= 0.0_r8) then
+      if (is_IOP) print '("ERROR: norm is nonzero")'
+      status = 1
+    end if
+
+  end subroutine test_mixed
+
+
+  !! Test the global consistency of the HT_2d_norm type
+  subroutine test_global(disc, mmf)
+
+    type(mfd_2d_disc), target, intent(in) :: disc
+    type(matl_mesh_func), target, intent(in) :: mmf
+
+    type(HT_2d_model), target :: HT_model
+    type(HT_2d_norm), target :: HT_norm
+    type(parameter_list), pointer :: params, sublist
+    real(r8), allocatable :: u(:), du(:)
+    character(:), allocatable :: errmsg, string
+    real(r8) :: du_norm, maxnorm, minnorm
+    integer :: n, stat
+
+    if (is_IOP) print '(/,"Testing global consistency")'
+
+    !! Define problem parameters
+    string = &
+    '{"model": { &
+        "bc": { &
+          "all-sides": { &
+            "type": "temperature", &
+            "face-set-ids": [1,2,3,4], &
+            "temp": 0.0}}}, &
+      "error-norm": { &
+        "temp-abs-tol": 1.0, &
+        "enth-abs-tol": 1.0}}'
+    call parameter_list_from_json_string(string, params, errmsg)
+    if (.not. associated(params)) call error_exit(errmsg)
+
+    !! Initialize 2D HT model
+    sublist => params%sublist('model')
+    call HT_model%init(disc, mmf, sublist, stat, errmsg)
+    if (stat /= 0) call error_exit(errmsg)
+
+    !! Initialize 2D HT norm
+    sublist => params%sublist('error-norm')
+    call HT_norm%init(HT_model, sublist)
+
+    !! Compute norm
+    n = HT_model%num_dof()
+    allocate(u(n), du(n))
+    u = 0.0_r8
+    du = this_PE
+    call HT_norm%compute(u, du, du_norm)
+
+    !! Check result
+    maxnorm = global_maxval(du_norm)
+    minnorm = global_minval(du_norm)
+    if (maxnorm /= minnorm) then
+      if (is_IOP) print '("ERROR: max and min norms do not match")'
+      status = 1
+    end if
+    if (du_norm /= nPE) then
+      if (is_IOP) print '("ERROR: du_norm is not the maximum rank; du_norm = ",es10.3)', du_norm
+      status = 1
+    end if
+
+  end subroutine test_global
+
+end program test_HT_2d_norm_type
