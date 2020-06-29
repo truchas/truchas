ad9cbf2757fc11b313a08c9d55fa0a1c9528c6aa
Neil N. Carlson <nnc@lanl.gov>
2020-06-29 00:03:35 -0600

    Add regression test for moving view factor radiation

---
+++ test/vfrad-moving/test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import truchas
+import os
+import numpy
+
+def run_test(tenv):
+    nfail = 0
+    stdout, output = tenv.truchas(4, "test.inp")
+    filename = os.path.join(output.directory, "probe.dat")
+    dataA = numpy.loadtxt(filename)
+    filename = os.path.join(tenv._input_dir, "golden.dat")
+    dataB = numpy.loadtxt(filename)
+    nfail += truchas.compare_max_rel(dataA[:,1], dataB[:,1], 1e-8, 'temperature', -1.0)
+    return nfail
+
+if __name__=="__main__":
+    tenv = truchas.TruchasEnvironment.default()
+    nfail = run_test(tenv)
+    assert nfail == 0
