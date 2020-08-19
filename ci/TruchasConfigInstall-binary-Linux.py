953faed1ff92cc97bf166b1f04f125470b1ecee5
Ondřej Čertík <ondrej@certik.us>
2020-08-19 13:29:57 -0600

    Use relocatable TruchasConfigInstall for Linux

---
+++ ci/TruchasConfigInstall-binary-Linux.py
@@ -0,0 +1,16 @@
+import os
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.realpath(this_dir + "/../../../..")
+
+class TruchasConfig:
+    mpiexec = root_dir + "/bin/mpiexec"
+
+    bin_dir = root_dir + "/bin"
+    lib_dir = root_dir + "/lib"
+    lib_suffix = ".so"
+
+    truchas_executable = os.path.join(bin_dir, "t-linux.x86_64.intel")
+    write_restart_executable = os.path.join(bin_dir, "write-restart.py")
+    libfwrite = os.path.join(lib_dir, "libfwrite" + lib_suffix)
+    libgridmap = os.path.join(lib_dir, "libgridmap" + lib_suffix)
