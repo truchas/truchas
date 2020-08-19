597740186bb7c20c9c68bb147be43626522177ba
Ondřej Čertík <ondrej@certik.us>
2020-08-19 11:51:11 -0600

    Ensure commands succeed in the make_dist script

--- ci/make_dist.py
+++ ci/make_dist.py
@@ -19,6 +19,10 @@ print("Version:", version)
 # Name of the distribution directory / tarball
 dist="truchas-%s-Linux" % version
 
+def run(x):
+    r = os.system(x)
+    if r != 0:
+        raise Exception("The command '%s' failed." % x)
 
 def copy_deps(binary_executable):
     os.system("""\
@@ -57,24 +61,24 @@ def copy_deps(binary_executable):
 
 
 
-os.system("rm -rf %s" % dist)
-os.system("mkdir -p %s/bin" % dist)
-os.system("mkdir -p %s/lib" % dist)
+run("rm -rf %s" % dist)
+run("mkdir -p %s/bin" % dist)
+run("mkdir -p %s/lib" % dist)
 
 # Copy the main binary and mpich executables
-os.system("cp %s %s/bin" % (tbin, dist))
-os.system("cp genre %s/bin" % (dist))
-os.system("cp vizre %s/bin" % (dist))
-os.system("cp %s/bin/mpiexec.hydra %s/bin/mpiexec" % (mpich_root, dist))
-os.system("cp %s/bin/hydra_pmi_proxy %s/bin/" % (mpich_root, dist))
+run("cp %s %s/bin" % (tbin, dist))
+run("cp genre %s/bin" % (dist))
+run("cp vizre %s/bin" % (dist))
+run("cp %s/bin/mpiexec.hydra %s/bin/mpiexec" % (mpich_root, dist))
+run("cp %s/bin/hydra_pmi_proxy %s/bin/" % (mpich_root, dist))
 
 # Copy an example
-os.system("mkdir -p %s/examples/broken-dam" % dist)
-os.system("cp %s/test/broken-dam/broken-dam.inp %s/examples/broken-dam/" % \
+run("mkdir -p %s/examples/broken-dam" % dist)
+run("cp %s/test/broken-dam/broken-dam.inp %s/examples/broken-dam/" % \
         (root_dir, dist))
 
 # Copy a README
-os.system("cp %s/ci/README-binary-Linux.md %s/README.md" % \
+run("cp %s/ci/README-binary-Linux.md %s/README.md" % \
         (root_dir, dist))
 
 # Copy all dependencies and set rpath properly
@@ -84,11 +88,11 @@ for b in [tbin, "genre", "vizre", "mpiexec", "hydra_pmi_proxy"]:
 
 # Remove libraries that we need to use from the system
 for lib in ["libc", "libm", "libpthread", "libdl", "librt"]:
-    os.system("rm %s/lib/%s.so.*" % (dist, lib))
+    run("rm %s/lib/%s.so.*" % (dist, lib))
 
 # Create a distribution tarball
-os.system("tar cjf %s.tar.bz2 %s" % (dist, dist))
+run("tar cjf %s.tar.bz2 %s" % (dist, dist))
 
 # Print the contents of the tarball
 
-os.system("tar tjf %s.tar.bz2" % dist)
+run("tar tjf %s.tar.bz2" % dist)
