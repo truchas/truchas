225741eef5b48dd881c32ec9efed844360933598
Zach Jibben <zjibben@lanl.gov>
2020-08-25 16:33:27 -0600

    WIP: Refactor solid mechanics package

---
+++ src/truchas/solid_mechanics/sm_namelist.F90
@@ -0,0 +1,109 @@
+!!
+!! Zach Jibben <zjibben@lanl.gov>
+!! August 2020
+!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!
+!! This file is part of Truchas. 3-Clause BSD license; see the LICENSE file.
+!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+#include "f90_assert.fpp"
+
+module sm_namelist
+
+  implicit none
+  private
+
+  public :: read_sm_namelist
+
+contains
+
+  subroutine read_sm_namelist(lun, params)
+
+    use,intrinsic :: iso_fortran_env, only: r8 => real64
+    use parallel_communication, only: is_IOP, broadcast
+    use input_utilities, only: seek_to_namelist, NULL_R, NULL_I
+    use string_utilities, only: i_to_c
+    use truchas_logging_services
+    use parameter_list_type
+
+    integer, intent(in) :: lun
+    type(parameter_list), intent(out) :: params
+    
+    real(r8) :: contact_distance, contact_norm_trac, contact_penalty
+    real(r8) :: strain_limit ! Maximum plastic strain increment
+    real(r8) :: convergence_criterion, nlk_vector_tolerance, relaxation_parameter
+    integer :: maximum_iterations, nlk_max_vectors, preconditioning_steps
+    namelist /solid_mechanics/ contact_distance, contact_norm_trac, contact_penalty, strain_limit, &
+        convergence_criterion, maximum_iterations, nlk_vector_tolerance, &
+        nlk_max_vectors, preconditioning_steps, relaxation_parameter
+
+    integer :: ios
+    logical :: found
+    character(80) :: iom
+    type(parameter_list), pointer :: plist => null()
+
+    call TLS_info('')
+    call TLS_info('Reading SM namelist ...')
+
+    !! Locate the SOLID_MECHANICS_NEW namelist (required)
+    if (is_IOP) then
+      rewind(lun)
+      call seek_to_namelist(lun, 'solid_mechanics_new', found, iostat=ios)
+    end if
+    call broadcast(ios)
+    if (ios /= 0) call TLS_fatal('error reading input file: iostat=' // i_to_c(ios))
+    call broadcast(found)
+    if (.not.found) call TLS_fatal('SOLID_MECHANICS namelist not found')
+
+    !! Default values
+    contact_distance = NULL_R
+    contact_norm_trac = NULL_R
+    contact_penalty = NULL_R
+    strain_limit = NULL_R
+
+    convergence_criterion = NULL_R
+    maximum_iterations = NULL_I
+    nlk_vector_tolerance = NULL_R
+    nlk_max_vectors = NULL_I
+    preconditioning_steps = NULL_I
+    relaxation_parameter = NULL_R
+
+    !! Read the namelist
+    if (is_IOP) read(lun,nml=solid_mechanics,iostat=ios,iomsg=iom)
+    call broadcast(ios)
+    if (ios /= 0) call TLS_fatal('error reading SOLID_MECHANICS namelist: ' // trim(iom))
+
+    !! Broadcast the namelist variables
+    call broadcast(contact_distance)
+    call broadcast(contact_norm_trac)
+    call broadcast(contact_penalty)
+    call broadcast(strain_limit)
+
+    call broadcast(maximum_iterations)
+    call broadcast(convergence_criterion)
+    call broadcast(nlk_max_vectors)
+    call broadcast(nlk_vector_tolerance)
+    call broadcast(preconditioning_steps)
+    call broadcast(relaxation_parameter)
+
+    plist => params%sublist('nonlinear-solver')
+    if (maximum_iterations /= NULL_I) call plist%set('nlk-max-iter', maximum_iterations)
+    if (convergence_criterion /= NULL_R) call plist%set('nlk-tol', convergence_criterion)
+    if (nlk_max_vectors /= NULL_I) call plist%set('nlk-max-vec', nlk_max_vectors)
+    if (nlk_vector_tolerance /= NULL_R) call plist%set('nlk-vec-tol', nlk_vector_tolerance)
+
+    plist => params%sublist('preconditioner')
+    if (preconditioning_steps /= NULL_I) call plist%set('num-iter', preconditioning_steps)
+    if (relaxation_parameter /= NULL_R) call plist%set('relaxation-parameter', relaxation_parameter)
+
+    plist => params%sublist('model')
+    if (contact_distance /= NULL_R) call plist%set('contact-distance', contact_distance)
+    if (contact_norm_trac /= NULL_R) call plist%set('contact-norm-trac', contact_norm_trac)
+    if (contact_penalty /= NULL_R) call plist%set('contact-penalty', contact_penalty)
+    if (strain_limit /= NULL_R) call plist%set('strain-limit', strain_limit)    
+
+  end subroutine read_sm_namelist
+
+end module sm_namelist
--- src/truchas/solid_mechanics/solid_mechanics_driver.F90
+++ src/truchas/solid_mechanics/solid_mechanics_driver.F90
@@ -15,9 +15,12 @@ module solid_mechanics_driver
   use,intrinsic :: iso_fortran_env, only: r8 => real64
   use parameter_list_type
   use truchas_logging_services
+  use unstr_mesh_type
   use solid_mechanics_type
   implicit none
   private
+
+  logical, parameter, public :: solid_mechanics_enabled = .true.
 
   public :: solid_mechanics_init
   public :: solid_mechanics_step
@@ -25,8 +28,24 @@ module solid_mechanics_driver
   public :: solid_mechanics_stress_view
   public :: solid_mechanics_displacement_view
 
-  type(solid_mechanics), target :: this
-  type(parameter_list) :: params
+  !! Bundled up all the driver state
+  type :: solid_mechanics_data
+    type(unstr_mesh), pointer :: mesh => null() ! reference only -- not owned
+    type(solid_mechanics) :: sm
+    type(parameter_list) :: params
+
+    ! matl_phase indirection
+    integer :: nphase
+    integer, allocatable :: matl_phase(:)
+
+    real(r8), allocatable :: vof(:,:) ! volume fractions for solids
+
+    ! The SM driver shouldn't need this but temperature is currently stored in
+    ! Zone%Temp so we need to keep a version on the new mesh as well
+    real(r8), allocatable :: temperature_cc(:)
+  end type solid_mechanics_data
+
+  type(solid_mechanics_data), target :: this
 
 contains
 
@@ -39,9 +58,9 @@ contains
 
     type(parameter_list), pointer :: plist => null()
 
-    call read_sm_namelist(lun, params)
+    call read_sm_namelist(lun, this%params)
 
-    plist => params%sublist('model')
+    plist => this%params%sublist('model')
     call plist%set('body-force-density', body_force_density)
 
   end subroutine solid_mechanics_build_parameter_list
@@ -55,42 +74,53 @@ contains
     use scalar_func_containers
     use tm_density
 
-    integer :: m, nmat, stat
+    integer :: p, m, stat
     real(r8) :: ref_dens, ref_temp
     character(:), allocatable :: errmsg
     type(scalar_func_box), allocatable :: lame1(:), lame2(:), density(:)
     class(scalar_func), allocatable :: cte
 
-    !! NB: Might need material indirection here.
-    allocate(lame1(nmat), lame2(nmat), density(nmat))
-    do m = 1, nmat
+    this%mesh => unstr_mesh_ptr('MAIN')
+    call init_phase_table
+
+    allocate(this%vof(this%nphase,this%mesh%ncell), this%temperature_cc(this%mesh%ncell))
+    
+    allocate(lame1(this%nphase), lame2(this%nphase), density(this%nphase))
+    do p = 1, this%nphase
+      m = this%matl_phase(p)
       ref_dens = matl_model%const_phase_prop(m, 'tm-ref-density')
       ref_temp = matl_model%const_phase_prop(m, 'tm-ref-temp')
       call matl_model%alloc_phase_prop(m, 'tm-linear-cte', cte)
       ASSERT(allocated(cte))
-      call alloc_tm_density_func(density(m)%f, ref_dens, ref_temp, cte, stat, errmsg)
+      call alloc_tm_density_func(density(p)%f, ref_dens, ref_temp, cte, stat, errmsg)
       if (stat /= 0) call tls_fatal("SOLID MECHANICS ERROR: tm-linear-cte -- " // errmsg)
 
-      call matl_model%alloc_phase_prop(m, 'tm-lame1', lame1(m)%f)
-      call matl_model%alloc_phase_prop(m, 'tm-lame2', lame2(m)%f)
-      ASSERT(allocated(lame1(m)%f))
-      ASSERT(allocated(lame2(m)%f))
+      call matl_model%alloc_phase_prop(m, 'tm-lame1', lame1(p)%f)
+      call matl_model%alloc_phase_prop(m, 'tm-lame2', lame2(p)%f)
+      ASSERT(allocated(lame1(p)%f))
+      ASSERT(allocated(lame2(p)%f))
     end do
 
-    call this%init(unstr_mesh_ptr('MAIN'), params, nmat, lame1, lame2, density)
+    call this%sm%init(this%mesh, this%params, this%nphase, lame1, lame2, density)
     
   end subroutine solid_mechanics_init
 
 
-  subroutine solid_mechanics_step(t, dt, vof, temperature_cc)
+  subroutine solid_mechanics_step(t, dt)
+
+    use zone_module, only: zone
+    use index_partitioning, only: gather_boundary
 
     real(r8), intent(in) :: t, dt
-    real(r8), intent(in) :: vof(:,:), temperature_cc(:)
 
     integer :: stat
     character(:), allocatable :: errmsg
 
-    call this%step(t, dt, vof, temperature_cc, stat, errmsg)
+    this%temperature_cc(1:this%mesh%ncell_onP) = Zone%Temp
+    call gather_boundary(this%mesh%cell_ip, this%temperature_cc)
+    call get_vof(this%vof)    
+
+    call this%sm%step(t, dt, this%vof, this%temperature_cc, stat, errmsg)
     if (stat /= 0) call tls_fatal(errmsg)
     
   end subroutine solid_mechanics_step
@@ -98,17 +128,59 @@ contains
 
   function solid_mechanics_displacement_view() result(view)
     real(r8), pointer :: view(:)
-    view => this%displacement_view()
+    view => this%sm%displacement_view()
   end function
 
   function solid_mechanics_strain_view() result(view)
     real(r8), pointer :: view(:,:)
-    view => this%strain_view()
+    view => this%sm%strain_view()
   end function
 
   function solid_mechanics_stress_view() result(view)
     real(r8), pointer :: view(:,:)
-    view => this%stress_view()
+    view => this%sm%stress_view()
   end function
+
+
+  !! PHASE_TABLE routines.
+  !! These routines interface with the matl_model to provide volume fractions
+  !! and a mapping between the matl_model and the phases solid mechanics cares
+  !! about (solids).
+
+  subroutine init_phase_table()
+
+    use material_model_driver, only: matl_model
+
+    integer :: m, p
+    
+    this%nphase = count(.not.matl_model%is_fluid)
+    allocate(this%matl_phase(this%nphase))
+
+    p = 1
+    do m = 1, matl_model%nphase_real
+      if (matl_model%is_fluid(m)) cycle
+      this%matl_phase(p) = m
+      p = p + 1
+    end do
+
+  end subroutine init_phase_table
+
+
+  subroutine get_vof(vof)
+
+    use matl_module, only: gather_vof
+    use index_partitioning, only: gather_boundary
+
+    real(r8), intent(out) :: vof(:,:)
+    integer :: p
+
+    ASSERT(size(vof,dim=1) == this%nphase)
+
+    do p = 1, this%nphase
+      call gather_vof(this%matl_phase(p), this%vof(p,:this%mesh%ncell_onP))
+    end do
+    call gather_boundary(this%mesh%cell_ip, vof)
+
+  end subroutine get_vof
 
 end module solid_mechanics_driver
