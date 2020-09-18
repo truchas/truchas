truchas/truchas

commit b6967db88a5ec8f52c1ea3c5f94e31089b8f4efb

Drop output of some marginally useful field data
This eliminates the output of cell centroids (which happened every time
snapshot despite the mesh not changing), density,  dT/dt, and grad T.

src/truchas/output/truchas_danu_output.F90
@@ -157,8 +157,8 @@ contains
     !! Flow-related fields.
     if (flow_enabled()) call write_new_flow_data
 
-    !! Heat transfer fields (other than temperature).
-    if (heat_transport) call write_heat_transfer_data
+    !! Heat transfer fields (other than temperature and enthalpy).
+    !if (heat_transport) call write_heat_transfer_data
 
     !! Induction heating fields.
     if (EM_is_on()) call write_EM_data
@@ -190,9 +190,9 @@ contains
       !! computed like that done by the call to density below, instead of just
       !! using the value in zone%rho; not sure why, as the computation is the
       !! same, but perhaps the zone%rho value is stale?  NNC, 8/9/2012.
-      allocate(rho(ncells))
-      call write_seq_cell_field (seq, zone%rho, 'Z_RHO', for_viz=.true., viz_name='Density')
-      deallocate(rho)
+      !allocate(rho(ncells))
+      !call write_seq_cell_field (seq, zone%rho, 'Z_RHO', for_viz=.true., viz_name='Density')
+      !deallocate(rho)
 
       !! Cell temperature
       call write_seq_cell_field (seq, zone%temp, 'Z_TEMP', for_viz=.true., viz_name='T')
@@ -212,12 +212,12 @@ contains
       end if
 
       !! Cell centroids
-      allocate(xc(ndim,ncells))
-      do j = 1, ncells
-        xc(:,j) = cell(j)%centroid
-      end do
-      call write_seq_cell_field (seq, xc, 'CENTROID', for_viz=.true., viz_name=['XC', 'YC', 'ZC'])
-      deallocate(xc)
+      !allocate(xc(ndim,ncells))
+      !do j = 1, ncells
+      !  xc(:,j) = cell(j)%centroid
+      !end do
+      !call write_seq_cell_field (seq, xc, 'CENTROID', for_viz=.true., viz_name=['XC', 'YC', 'ZC'])
+      !deallocate(xc)
 
     end subroutine write_common_data
 

commit 40f508927429e19c87294ade9498e8c5f4e6dbf6

Fix issues introduced by b6967db.
This restores output of the cell averaged mass density which is needed
for restarts (strangely enough). It also removes the check on the thermal
gradient (no longer output) in the remap test.

src/truchas/output/truchas_danu_output.F90
@@ -190,9 +190,9 @@ contains
       !! computed like that done by the call to density below, instead of just
       !! using the value in zone%rho; not sure why, as the computation is the
       !! same, but perhaps the zone%rho value is stale?  NNC, 8/9/2012.
-      !allocate(rho(ncells))
-      !call write_seq_cell_field (seq, zone%rho, 'Z_RHO', for_viz=.true., viz_name='Density')
-      !deallocate(rho)
+      allocate(rho(ncells))
+      call write_seq_cell_field (seq, zone%rho, 'Z_RHO', for_viz=.true., viz_name='Density')
+      deallocate(rho)
 
       !! Cell temperature
       call write_seq_cell_field (seq, zone%temp, 'Z_TEMP', for_viz=.true., viz_name='T')

test/restart/testRemap.py
@@ -29,19 +29,6 @@ def test_remap(tenv):
     print("{:s}: temperature: min {:8.2e} max {:8.2e} L2 {:8.2e}".format(status,min,max,l2))
     if max > tol: nfail += 1
 
-    # compare the final temperature gradients
-    field2 = output2.field(sid2, "Grad_T")
-    field4 = output4.field(sid4, "Grad_T")
-
-    max2 = npla.norm(field2,  np.inf)
-    max = npla.norm(field4 - field2,  np.inf)
-    min = npla.norm(field4 - field2, -np.inf)
-    l2  = npla.norm(field4 - field2)
-
-    status = "PASS" if max/max2 <= tol else "FAIL"
-    print("{:s}: grad_T: max of 2pe {:8.2e} min {:8.2e} max {:8.2e} L2 {:8.2e}".format(status,max2,min,max,l2))
-    if max/max2 > tol: nfail += 1
-
     return nfail
 
 if __name__=="__main__":