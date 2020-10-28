f2fa98430b96c3fcab37fe6d66c9487f824a0dfd
Zach Jibben <threeofsix@gmail.com>
2020-10-28 17:29:14 -0600

    Add new/legacy consistency test

---
+++ test/stretch/test-thermoelastic-consistency.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# This runs a series of comparisons between the new solid mechanics and old, in
+# thermoelastic (linear-only) configurations. It ensures both produce the same
+# results, within some tolerance. No golden data is used.
+
+import truchas
+
+
+def run_test(tenv):
+    nfail = 0
+
+    # stretch in the x direction, compare displacements, strains
+    for d in ("x", "y", "z"):
+        name = "stretch-n{:s}".format(d)
+        stdout1, output1 = tenv.truchas(4, "{:s}.inp".format(name))
+        stdout2, output2 = tenv.truchas(4, "{:s}-legacy.inp".format(name))
+        sid = 2
+        time = output1.time(sid)
+        out1 = output1.field(sid, "Displacement")
+        out2 = output2.field(sid, "Displacement")
+        nfail += truchas.compare_max(out1, out2, 1e-12, "{:s}-displacement".format(name), time)
+        # TODO: strain
+        # TODO: stress
+        print()
+
+    # TODO: thermal gradients
+    # TODO: shear BCs
+    # TODO: normal stress BCs
+    # TODO: off-axis rotations
+
+    truchas.report_summary(nfail)
+    return nfail
+
+
+if __name__=="__main__":
+    tenv = truchas.TruchasEnvironment.default()
+    nfail = run_test(tenv)
+    assert nfail == 0
