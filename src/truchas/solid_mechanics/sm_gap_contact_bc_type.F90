6943f0b19fa975ae786f4201774c135dc9e6c0f5
Zach Jibben <zjibben@gmail.com>
2020-11-10 18:30:43 -0700

    WIP: Add gap contact BC type
    This starts initializing the structure for the gap contact type. Still need to
    compute the appropriate values, rotation matrices such as in the normal BC
    types, and build a structure to map from link faces to link nodes. Also need to
    complete the residual terms in sm_model_type.

--- src/truchas/solid_mechanics/sm_gap_contact_bc_type.F90
+++ src/truchas/solid_mechanics/sm_gap_contact_bc_type.F90
@@ -13,43 +13,122 @@
 module sm_gap_contact_bc_type
 
   use,intrinsic :: iso_fortran_env, only: r8 => real64
+  use intfc_func2_class
   use unstr_mesh_type
-  use bndry_func1_class
-  use bndry_face_func_type
   use integration_geometry_type
   implicit none
   private
 
-  type, extends(bndry_func1), public :: sm_gap_contact_bc
+  type, extends(intfc_func2), public :: sm_gap_contact_bc
     private
     real(r8), public, allocatable :: rotation_matrix(:,:,:)
     logical, public :: enabled
 
-    type(bndry_face_func), allocatable :: bff
-    real(r8), allocatable :: area_ip(:)
-    integer, allocatable :: fini(:), xfini(:)
+    integer, allocatable :: lface_index(:)
+    ! type(bndry_face_func), allocatable :: bff
+    ! real(r8), allocatable :: area_ip(:)
+    ! integer, allocatable :: fini(:), xfini(:)
+
+    type(unstr_mesh), pointer :: mesh => null() ! reference only - do not own
+    type(integration_geometry), pointer :: ig => null() ! reference only - do not own
+    integer, allocatable :: xgroup(:)
+    type(scalar_func_box), allocatable :: f(:)
+    ! temporaries used during construction
+    type(intfc_link_group_builder), allocatable :: builder
+    type(scalar_func_list) :: flist
   contains
     procedure :: init
+    procedure :: add
+    procedure :: add_complete
     procedure :: compute
+    procedure :: compute_value => compute
+    procedure :: compute_deriv => compute
   end type sm_gap_contact_bc
 
 contains
 
-  subroutine init(this, mesh, ig, bff)
+  subroutine init(this, mesh, ig)
+    class(sm_gap_contact_bc), intent(out) :: this
+    type(unstr_mesh), intent(in), target :: mesh
+    type(integration_geometry), intent(in), target :: ig
+    this%mesh => mesh
+    this%ig => ig
+    allocate(this%builder)
+    call this%builder%init(mesh)
+  end subroutine init
+
+
+  subroutine add(this, func, setids, stat, errmsg)
+    class(sm_gap_contact_bc), intent(inout) :: this
+    class(scalar_func), allocatable, intent(inout) :: func
+    integer, intent(in) :: setids(:)
+    integer, intent(out) :: stat
+    character(:), allocatable, intent(out) :: errmsg
+    call this%builder%add_link_group(setids, stat, errmsg)
+    if (stat /= 0) return
+    call this%flist%append(func)
+  end subroutine add
+
+
+  subroutine add_complete(this)
 
     use parallel_communication, only: global_any
 
-    class(sm_gap_contact_bc), intent(out) :: this
-    type(unstr_mesh), intent(in), target :: mesh
-    type(integration_geometry), intent(in) :: ig
-    type(bndry_face_func), intent(inout), allocatable :: bff
+    class(sm_gap_contact_bc), intent(inout) :: this
 
-    !! TODO-WARN
-    allocate(this%index(0))
+    ASSERT(allocated(this%builder))
+    call this%builder%get_link_groups(this%ngroup, this%xgroup, this%lface_index)
+    deallocate(this%builder)
+    call scalar_func_list_to_box_array(this%flist, this%f)
+
+    call compute_index
+    allocate(this%value(size(this%index,dim=2)))
+    allocate(this%rotation_matrix(3,3,size(this%index,dim=2)))
 
     this%enabled = global_any(size(this%index) > 0)
-    
-  end subroutine init
+
+  contains
+
+    !! TODO: This BC is along nodes, specifically linked nodes. We have a list of
+    !! face links, from which we must get the node links. Such a structure
+    !! should probably go into integration geometry.
+    subroutine compute_index()
+
+      integer :: count, lfi, lf, xlfi, ln
+      logical :: touched(this%ig%nlnode)
+
+      count = 0
+      touched = .false.
+      do lfi = 1, size(this%lface_index)
+        lf = this%lface_index(lfi)
+        do xlfi = this%ig%xlnlface(lf), this%ig%xlnlf(lf+1)
+          ln = this%ig%lnlface(xlfi)
+          if (.not.touched(ln)) then
+            count = count + 1
+            touched(ln) = .true.
+          end if
+        end do
+      end do
+
+      allocate(this%index(2,count))
+
+      count = 0
+      touched = .false.
+      do lfi = 1, size(this%lface_index)
+        lf = this%lface_index(lfi)
+        do xlfi = this%ig%xlnlface(lf), this%ig%xlnlf(lf+1)
+          ln = this%ig%lnlface(xlfi)
+          if (.not.touched(ln)) then
+            count = count + 1
+            touched(ln) = .true.
+            this%index(:,count) = this%ig%lnode(:,ln)
+          end if
+        end do
+      end do
+
+    end subroutine compute_index
+
+  end subroutine add_complete
 
 
   !! TODO-WARN
