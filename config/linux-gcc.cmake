539e740e82cf4be212e3ca54d01c10d04e851fdf
Peter Brady <ptb@lanl.gov>
2022-03-25 14:34:22 -0600

    Enforce certain Intel and GCC options
    Intel: -standard-semantics
    GNU: -fimplicit-none -ffree-line-length
    
    -standard-semantics includes the -assume std_mod_proc_name option
    which impacts ABI in a very viral way.  To work around potential
    options, added a build option ENABLE_STD_MOD_PROC_NAME which defaults
    to OFF but should be set to whatever the TPLS were built with.

--- config/linux-gcc.cmake
+++ config/linux-gcc.cmake
@@ -4,13 +4,11 @@ set(CMAKE_C_COMPILER gcc CACHE STRING "C Compiler")
 set(CMAKE_Fortran_COMPILER gfortran CACHE STRING "Fortran Compiler")
 
 # Additional flags to the default CMAKE_<lang>_FLAGS_<build_type> flags
-set(Truchas_Fortran_FLAGS "-fimplicit-none -ffree-line-length-none"
-    CACHE STRING "Fortran compile flags")
-set(CMAKE_Fortran_FLAGS_RELEASE "${Truchas_Fortran_FLAGS} -O3 -DNDEBUG"
+set(CMAKE_Fortran_FLAGS_RELEASE "-O3 -DNDEBUG"
     CACHE STRING "Fortran compile flags")
 # -fcheck=bits is excluded here since it is not supported on GCC 9.3.0
-set(CMAKE_Fortran_FLAGS_DEBUG "${Truchas_Fortran_FLAGS} \
--g -O0 -fcheck=bounds,do,mem,pointer -finit-real=nan -finit-integer=-2147483647"
+set(CMAKE_Fortran_FLAGS_DEBUG
+  "-g -O0 -fcheck=bounds,do,mem,pointer -finit-real=nan -finit-integer=-2147483647"
     CACHE STRING "Fortran compile flags")
 set(CMAKE_Fortran_FLAGS_RELWITHDEBINFO "-g ${CMAKE_Fortran_FLAGS_RELEASE}"
     CACHE STRING "Fortran compile flags")
