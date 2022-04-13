6c90ce7d16f60cd2c994ad6148e1f52c79b1e572
Naren Raghavan <naren@chicoma.lanl.gov>
2022-04-13 13:57:56 -0600

    Upload to Neil to check the error

--- src/truchas/physics/additive_manufacturing/gauss_laser_irrad_type.F90
+++ src/truchas/physics/additive_manufacturing/gauss_laser_irrad_type.F90
@@ -16,6 +16,7 @@ module gauss_laser_irrad_type
 
   use laser_irrad_class
   use scalar_func_class
+  use scalar_func_factories
   use,intrinsic :: iso_fortran_env, only: r8 => real64
   implicit none
   private
@@ -36,7 +37,12 @@ contains
     class(gauss_laser_irrad), intent(out) :: this
     type(parameter_list) :: params
     real(r8) :: sigma
+    class(scalar_func), allocatable :: p      
+    character(:), allocatable :: errmsg
+
     call params%get('sigma', sigma)
+    call alloc_scalar_func(params, 'laser-power', p)
+    this%power = p
     this%c1 = 2.0_r8 * sigma**2   ! length^2
   end subroutine init
 
@@ -45,7 +51,7 @@ contains
     real(r8), intent(in) :: t, dx, dy, dz
     real(r8), parameter :: PI = 3.141592653589793_r8
     real(r8) :: irrad
-    irrad = this%power%eval([t]) / (PI * this%c1) * exp(-(dx**2 + dy**2)/this%c1)
+    !irrad = this%power%eval([t]) / (PI * this%c1) * exp(-(dx**2 + dy**2)/this%c1)
   end function irrad
 
 end module gauss_laser_irrad_type
