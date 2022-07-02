dc9c2708e507217b5d69f1e43d418c7b59ac71ae
Neil N. Carlson <neil.n.carlson@gmail.com>
2022-07-02 11:25:26 -0600

    Switch vtrack_driver to define_matl

--- src/truchas/physics/volume_tracking/vtrack_driver.F90
+++ src/truchas/physics/volume_tracking/vtrack_driver.F90
@@ -213,21 +213,21 @@ contains
   end subroutine get_vof_from_matl
 
   subroutine put_vof_into_matl()
-    use matl_utilities, only: matl_set_vof
+    use matl_utilities, only: define_matl
     integer :: i
     do i = 1, size(this%liq_matid)
       this%vof(this%liq_matid(i),:) = this%fvof_o(i,:this%mesh%ncell_onP)
     end do
-    call matl_set_vof(this%vof)
+    call define_matl(this%vof)
   end subroutine put_vof_into_matl
 
   subroutine put_orig_vof_into_matl
-    use matl_utilities, only: matl_set_vof
+    use matl_utilities, only: define_matl
     integer :: i
     do i = 1, size(this%liq_matid)
       this%vof(this%liq_matid(i),:) = this%fvof_i(i,:this%mesh%ncell_onP)
     end do
-    call matl_set_vof(this%vof)
+    call define_matl(this%vof)
   end subroutine
 
   ! vel_fn is the outward oriented face-normal velocity
