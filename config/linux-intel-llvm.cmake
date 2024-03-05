09f19ce84be549b9505f715b05a8b355e2665384
Zach Jibben <zjibben@lanl.gov>
2024-03-05 16:08:37 -0700

    LLVM cmake config and some small fixes

---
+++ config/linux-intel-llvm.cmake
@@ -0,0 +1,16 @@
+# Generic Linux with the Intel Compilers
+
+set(CMAKE_C_COMPILER icx CACHE STRING "C Compiler")
+set(CMAKE_Fortran_COMPILER ifx CACHE STRING "Fortran Compiler")
+
+# Additional flags to the default CMAKE_<lang>_FLAGS_<build_type> flags
+set(Truchas_Fortran_FLAGS "-u -traceback -fpe0")
+set(CMAKE_Fortran_FLAGS_RELEASE "${Truchas_Fortran_FLAGS} -O3 -DNDEBUG"
+    CACHE STRING "Fortran compile flags")
+set(CMAKE_Fortran_FLAGS_DEBUG "${Truchas_Fortran_FLAGS} \
+-O0 -g -check noshape,noarg_temp_created,bounds,contiguous,pointers \
+-init=arrays,snan"
+    CACHE STRING "Fortran compile flags")
+set(CMAKE_Fortran_FLAGS_RELWITHDEBINFO "-g ${CMAKE_Fortran_FLAGS_RELEASE}"
+    CACHE STRING "Fortran compile flags")
+# "-C -check uninit" flags leads to error
