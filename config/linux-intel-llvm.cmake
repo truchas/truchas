7ec28e897ec6c4f2a102c7d0884d20430d8f6718
Zach Jibben <zjibben@lanl.gov>
2024-03-06 16:56:06 -0700

    Disable FP exception trapping for HDF5
    This drops the trapping of floating point exceptions for the ifort
    and nagfor compilers, which causes failures in HDF5 1.14. It also
    restores some runtime checking flags for the ifx compiler that
    weren't necessary to remove to avoid the same failures.

--- config/linux-intel-llvm.cmake
+++ config/linux-intel-llvm.cmake
@@ -4,13 +4,13 @@ set(CMAKE_C_COMPILER icx CACHE STRING "C Compiler")
 set(CMAKE_Fortran_COMPILER ifx CACHE STRING "Fortran Compiler")
 
 # Additional flags to the default CMAKE_<lang>_FLAGS_<build_type> flags
-set(Truchas_Fortran_FLAGS "-u -traceback -fpe0")
+set(Truchas_Fortran_FLAGS "-u -traceback")
 set(CMAKE_Fortran_FLAGS_RELEASE "${Truchas_Fortran_FLAGS} -O3 -DNDEBUG"
     CACHE STRING "Fortran compile flags")
 set(CMAKE_Fortran_FLAGS_DEBUG "${Truchas_Fortran_FLAGS} \
--O0 -g -check noshape,noarg_temp_created,bounds,contiguous,pointers \
--init=arrays,snan"
+-O0 -g -check noshape,noarg_temp_created,bounds,contiguous,pointers"
     CACHE STRING "Fortran compile flags")
 set(CMAKE_Fortran_FLAGS_RELWITHDEBINFO "-g ${CMAKE_Fortran_FLAGS_RELEASE}"
     CACHE STRING "Fortran compile flags")
 # "-C -check uninit" flags leads to error
+# hdf5 errors: -fpe0 -init=arrays,snan
--- config/linux-intel.cmake
+++ config/linux-intel.cmake
@@ -4,12 +4,12 @@ set(CMAKE_C_COMPILER icc CACHE STRING "C Compiler")
 set(CMAKE_Fortran_COMPILER ifort CACHE STRING "Fortran Compiler")
 
 # Additional flags to the default CMAKE_<lang>_FLAGS_<build_type> flags
-set(Truchas_Fortran_FLAGS "-u -traceback -fpe0")
+set(Truchas_Fortran_FLAGS "-u -traceback")
 set(CMAKE_Fortran_FLAGS_RELEASE "${Truchas_Fortran_FLAGS} -O3 -DNDEBUG"
     CACHE STRING "Fortran compile flags")
 set(CMAKE_Fortran_FLAGS_DEBUG "${Truchas_Fortran_FLAGS} \
--O0 -C -check noshape,noarg_temp_created,bounds,uninit,contiguous,pointers \
--init=arrays,snan"
+-O0 -C -check noshape,noarg_temp_created,bounds,uninit,contiguous,pointers"
     CACHE STRING "Fortran compile flags")
 set(CMAKE_Fortran_FLAGS_RELWITHDEBINFO "-g ${CMAKE_Fortran_FLAGS_RELEASE}"
     CACHE STRING "Fortran compile flags")
+# hdf5 errors: -fpe0 -init=arrays,snan
