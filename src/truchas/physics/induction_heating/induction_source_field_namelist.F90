43d56d8a848394929ab03b553e1edadec120ac67
Neil N. Carlson <neil.n.carlson@gmail.com>
2024-03-13 10:52:44 -0600

    Merge EM namelists
    This merges the induction_coil namelist into the new
    induction_source_field namelist. Reference manual updated.

--- src/truchas/physics/induction_heating/induction_source_field_namelist.F90
+++ src/truchas/physics/induction_heating/induction_source_field_namelist.F90
@@ -12,23 +12,30 @@ contains
 
     use,intrinsic :: iso_fortran_env, only: r8 => real64
     use string_utilities, only: i_to_c
-    use input_utilities, only: seek_to_namelist, NULL_C, NULL_R
+    use input_utilities, only: seek_to_namelist, NULL_I, NULL_C, NULL_R
     use parallel_communication, only: is_IOP, broadcast
     use truchas_logging_services
 
     integer, intent(in) :: lun
     type(parameter_list), intent(inout) :: params
 
-    integer :: n, ios
+    integer :: n, ios, j
     logical :: found
     character(128) :: iom
+    character(:), allocatable :: pname, vname
     real(r8), allocatable :: array(:)
+    type(parameter_list), pointer :: coils_plist, plist
 
     !! Namelist variables
     character :: orientation
     integer, parameter :: M = 32
     real(r8) :: times(M), frequency(M+1), uniform_strength(M+1)
-    namelist /induction_source_field/ orientation, times, frequency, uniform_strength
+    type :: induction_coil
+      integer :: num_loops
+      real(r8) :: center(3), radius, length, current(M+1)
+    end type
+    type(induction_coil) :: coils(32)
+    namelist /induction_source_field/ orientation, times, frequency, uniform_strength, coils
 
     call TLS_info('Reading INDUCTION_SOURCE_FIELD namelist ...')
 
@@ -44,6 +51,7 @@ contains
     times = NULL_R
     frequency = NULL_R
     uniform_strength = NULL_R
+    call set_coils_default
 
     if (is_IOP) read(lun,nml=induction_source_field,iostat=ios,iomsg=iom)
     call broadcast(ios)
@@ -53,8 +61,7 @@ contains
     call broadcast(times)
     call broadcast(frequency)
     call broadcast(uniform_strength)
-
-    !! Parameters defining the external applied magnetic field !!!!!!!!!!!!!!!!!
+    call broadcast_coils
 
     select case (orientation)
     case ('x','X','y','Y','z','Z')
@@ -93,6 +100,89 @@ contains
       end if
       call params%set('uniform-strength', array) !TODO: separate scalar and array cases?
     end if
+
+    coils_plist => params%sublist('coils')
+    do j = 1, size(coils)
+      if (default_coil(coils(j))) cycle ! unused element
+      plist => coils_plist%sublist('COIL' // i_to_c(j))
+
+      pname = 'COILS(' // i_to_c(j) // ')%'
+      vname = pname // 'NUM_LOOPS'
+      if (coils(j)%num_loops == NULL_I) then
+        call TLS_fatal(vname // ' must be assigned a value')
+      else if (coils(j)%num_loops <= 0) then
+        call TLS_fatal(vname // ' must be > 0')
+      end if
+      call plist%set('num-loops', coils(j)%num_loops)
+
+      vname = pname // 'RADIUS'
+      if (coils(j)%radius == NULL_R) then
+        call TLS_fatal(vname // ' must be assigned a value')
+      else if (coils(j)%radius <= 0.0_r8) then
+        call TLS_fatal(vname // ' must be > 0.0')
+      end if
+      call plist%set('radius', coils(j)%radius)
+
+      vname = pname // 'LENGTH'
+      if (coils(j)%num_loops > 1) then
+        if (coils(j)%length == NULL_R) then
+          call TLS_fatal(vname // ' must be assigned a value')
+        else if (coils(j)%length < 0.0_r8) then
+          call TLS_fatal(vname // ' must be >= 0.0')
+        end if
+        call plist%set('length', coils(j)%length)
+      end if
+
+      vname = pname // 'CENTER'
+      if (any(coils(j)%center /= NULL_R)) then
+        if (any(coils(j)%center == NULL_R)) call TLS_fatal(vname // ' requires 3 values')
+        call plist%set('center', coils(j)%center)
+      end if
+
+      vname = pname // 'CURRENT'
+      array = pack(coils(j)%current, mask=(coils(j)%current/=NULL_R))
+      if (size(array) == 0) then
+        call TLS_fatal(vname // ' must be assigned a value')
+      else if (size(array) /= n+1) then
+        call TLS_fatal(i_to_c(n+1) // ' values required for ' // vname)
+      end if
+      call plist%set('current', array)
+    end do
+
+  contains
+
+    subroutine set_coils_default
+      integer :: j
+      do j = 1, size(coils)
+        coils(j)%num_loops = NULL_I
+        coils(j)%center = NULL_R
+        coils(j)%radius = NULL_R
+        coils(j)%length = NULL_R
+        coils(j)%current = NULL_R
+      end do
+    end subroutine
+
+    subroutine broadcast_coils
+      integer :: j
+      do j = 1, size(coils)
+        call broadcast(coils(j)%num_loops)
+        call broadcast(coils(j)%center)
+        call broadcast(coils(j)%radius)
+        call broadcast(coils(j)%length)
+        call broadcast(coils(j)%current)
+      end do
+    end subroutine
+
+    logical function default_coil(coil)
+      type(induction_coil), intent(in) :: coil
+      default_coil = .false.
+      if (coil%num_loops /= NULL_I) return
+      if (any(coil%center /= NULL_R)) return
+      if (coil%radius /= NULL_R) return
+      if (coil%length /= NULL_R) return
+      if (any(coil%current /= NULL_R)) return
+      default_coil = .true.
+    end function
 
   end subroutine read_induction_source_field_namelist
 
