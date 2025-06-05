f906d0579de45ec2cfe0eb10768473195de887bb
Neil N. Carlson <neil.n.carlson@gmail.com>
2025-06-05 17:42:17 -0600

    Delete fdme_precon_class.F90
    This eliminates the abstract fdme_precon class and makes direct
    use of the pcsr_precon class within the fdme_minres_solver type
    as it is now the only type of preconditioner.

--- src/truchas/physics/induction_heating/fdme_minres_solver_type.F90
+++ src/truchas/physics/induction_heating/fdme_minres_solver_type.F90
@@ -1,38 +1,56 @@
+!!
+!! FDME_MINRES_SOLVER
+!!
+!! A solver for the frequency-domain Maxwell equations that solves the linear
+!! system iteratively using the preconditioned CS-MINRES method for complex
+!! symmetric systems.
+!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 #include "f90_assert.fpp"
 
 module fdme_minres_solver_type
 
   use,intrinsic :: iso_fortran_env, only: r8 => real64
   use complex_lin_op_class
-  use imap_zvector_type
   use fdme_model_type
-  use fdme_precon_class
   use cs_minres_solver_type
+  use pcsr_precon_class
+  use pcsr_matrix_type
+  use imap_zvector_type
   implicit none
   private
 
   type, extends(complex_lin_op), public :: fdme_minres_solver
     type(fdme_model), pointer :: model => null() ! unowned reference
-    class(fdme_precon), allocatable :: my_precon
     type(cs_minres_solver) :: minres
+    class(pcsr_precon), allocatable :: pc
+    type(pcsr_matrix), pointer :: pc_mtx => null() ! pointer to avoid dangling pointer
     type(imap_zvector) :: efield, rhs
+    real(r8) :: beta
   contains
     procedure :: init
     procedure :: solve
     ! deferred procedures from complex_lin_op class
     procedure :: matvec
     procedure :: precon
+    final :: fdme_minres_solver_delete
   end type
 
 contains
 
+  impure elemental subroutine fdme_minres_solver_delete(this)
+    type(fdme_minres_solver), intent(inout) :: this
+    if (associated(this%pc_mtx)) deallocate(this%pc_mtx)
+  end subroutine
+
   subroutine init(this, model, params, stat, errmsg)
 
     use parameter_list_type
-    use fdme_precon_pcsr_type
+    use pcsr_precon_factory, only: alloc_pcsr_precon
 
     class(fdme_minres_solver), intent(out) :: this
-    type(fdme_model), pointer :: model !TODO: don't make a pointer
+    type(fdme_model), target :: model
     type(parameter_list), intent(inout) :: params
     integer, intent(out) :: stat
     character(:), allocatable, intent(out) :: errmsg
@@ -42,46 +60,56 @@ contains
 
     this%model => model
     call this%efield%init(model%mesh%edge_imap) ! initialized to 0
-    call this%rhs%init(model%mesh%edge_imap)
+    call this%rhs%init(mold=this%efield)
     call this%minres%init(params)
 
     plist => params%sublist('precon')
-    call plist%get('type', precon_type, stat, errmsg, default='gs')
+    call plist%get('type', precon_type, stat, errmsg, default='ssor')
     if (stat /= 0) return
-    select case (precon_type)
-    case ('boomer','ssor') ! Hypre BoomerAMG
-      allocate(fdme_precon_pcsr :: this%my_precon)
-      call this%my_precon%init(model, plist, stat, errmsg)
-    case ('none')
-    case default
-      stat = 1
-      errmsg = 'invalid type value: ' // precon_type
-      return
-    end select
+    if (precon_type == 'none') return
+
+    !! Create the preconditioner
+    allocate(this%pc_mtx)
+    call this%pc_mtx%init(this%model%A%graph, take_graph=.false.)
+    call plist%get('beta', this%beta, stat, errmsg, default=0.0_r8)
+    if (stat /= 0) return
+    call alloc_pcsr_precon(this%pc, this%pc_mtx, plist, stat, errmsg)
     if (stat /= 0) return
 
-  end subroutine
+  end subroutine init
 
   subroutine solve(this, efield, stat, errmsg)
-    use string_utilities, only: i_to_c
+
     class(fdme_minres_solver), intent(inout) :: this
     complex(r8), intent(inout) :: efield(:)
     integer, intent(out) :: stat
     character(:), allocatable, intent(out) :: errmsg
+
     character(:), allocatable :: msg
+
+    !! Setup the preconditioner
+    if (allocated(this%pc)) then
+      this%pc_mtx%values(:) = this%model%A%values%re + this%beta * this%model%M%values%re
+      call this%pc%compute
+    end if
+
     this%rhs%v(:) = this%model%rhs
-    if (allocated(this%my_precon)) call this%my_precon%setup
+
     call this%minres%solve(this, this%rhs, this%efield, stat, msg)
-    !TODO: add solver summary output
     stat = merge(0, 1, stat >= 0) ! for minres stat >= 0 is success and < 0 failure
     if (stat /= 0) errmsg = msg
+    call this%efield%gather_offp
     efield(:) = this%efield%v
-  end subroutine
+
+  end subroutine solve
 
   subroutine matvec(this, x, y)
+
     use zvector_class
+
     class(fdme_minres_solver), intent(inout) :: this
     class(zvector) :: x, y
+
     select type (x)
     type is (imap_zvector)
       select type (y)
@@ -90,25 +118,43 @@ contains
         call this%model%A%matvec(x%v, y%v)
       end select
     end select
-  end subroutine
+
+  end subroutine matvec
 
   subroutine precon(this, x, y)
+
     use zvector_class
+
     class(fdme_minres_solver), intent(inout) :: this
     class(zvector) :: x, y
-    if (allocated(this%my_precon)) then
-      select type (x)
+
+    call y%copy(x)
+    if (.not.allocated(this%pc)) return
+
+    select type (x)
+    type is (imap_zvector)
+      select type (y)
       type is (imap_zvector)
-        select type (y)
-        type is (imap_zvector)
-          call x%gather_offp
-          call this%my_precon%apply(x%v, y%v)
-          call y%gather_offp ! necessary?
-        end select
+        call y%gather_offp
+#if defined(INTEL_BUG20250605) || defined(GNU_PR119986)
+        call workaround(y%v)
+#else
+        call this%pc%apply(y%v%re)
+        call this%pc%apply(y%v%im)
+#endif
       end select
-    else ! no preconditioning
-      call y%copy(x)
-    end if
-  end subroutine
+    end select
+
+#if defined(INTEL_BUG20250605) || defined(GNU_PR119986)
+  contains
+
+    subroutine workaround(z)
+      complex(r8), intent(inout) :: z(:)
+      call this%pc%apply(z%re)
+      call this%pc%apply(z%im)
+    end subroutine
+#endif
+
+  end subroutine precon
 
 end module fdme_minres_solver_type
