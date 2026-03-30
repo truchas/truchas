020cb047c8c50817bb37c40c301a48324b969830
Zach Jibben <zjibben@lanl.gov>
2026-03-30 17:08:35 -0600

    Update TruchasEMData for parallel EM VTKHDF output

--- test/em4/em.py
+++ test/em4/em.py
@@ -36,7 +36,7 @@ def get_heat_source(tdata):
 
 
 def get_Emag(tdata):
-    temdata = tdata.em_data("Group_0")
+    temdata = tdata.em_data("stuff")
     xc = temdata.centroids()
     r = np.sqrt(xc[:,0]**2 + xc[:,1]**2)
     Emagv = temdata.field("|E|")
