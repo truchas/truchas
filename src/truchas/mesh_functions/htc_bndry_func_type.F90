cce96182333e365c1cd9fdccd1d813bcdb8c841d
Neil N. Carlson <neil.n.carlson@gmail.com>
2026-07-18 16:12:08 -0600

    Support temperature-dependent boundary HTC
    Evaluate external HTC functions with the local boundary temperature and include the coefficient derivative in the diffusion preconditioner. Document the updated function arguments and exercise them with an analytic steady-state regression.

--- src/truchas/mesh_functions/htc_bndry_func_type.F90
+++ src/truchas/mesh_functions/htc_bndry_func_type.F90
@@ -1,18 +1,23 @@
 !!
 !! HTC_BNDRY_FUNC_TYPE
 !!
-!! This module defines an extension of the base class BNDRY_FIELD_FUNC that
-!! implements the HTC boundary condition flux function on a subset of the
-!! boundary faces of a mesh type that extends the UNSTR_BASE_MESH class.
+!! This module defines a heat-transfer-coefficient boundary flux function on a
+!! subset of mesh boundary faces. The function stores the sparse face set and
+!! evaluates user-supplied HTC and ambient-temperature scalar functions for
+!! thermal transport models.
 !!
-!! Neil N. Carlson <nnc@lanl.gov>
-!! December 2017
+!! Neil Carlson <neil.n.carlson@gmail.com>, July 2026
+!! SPDX-License-Identifier: BSD-3-Clause
 !!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!! Notes
 !!
-!! This file is part of Truchas. 3-Clause BSD license; see the LICENSE file.
+!! The returned flux value is area*h*(T-Tamb). The HTC function h receives
+!! anonymous arguments (T,time,x,y,z), while the ambient-temperature function
+!! receives (time,x,y,z).
 !!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!! COMPUTE_DERIV returns the derivative with respect to T. It includes a
+!! centered finite-difference approximation of the temperature dependence of h
+!! but treats Tamb as independent of T.
 
 #include "f90_assert.fpp"
 
@@ -80,19 +85,23 @@ contains
     real(r8), intent(in) :: u(:)
     real(r8), allocatable, intent(out) :: value(:)
     integer :: n, j
-    real(r8) :: args(0:size(this%mesh%x,dim=1)), c
+    real(r8) :: args(-1:size(this%mesh%x,dim=1))
+    real(r8) :: args0(0:size(this%mesh%x,dim=1)), c
     ASSERT(allocated(this%index))
     allocate(value(size(this%index)))
     args(0) = t
+    args0(0) = t
     do n = 1, this%ngroup
       associate(index => this%index(this%xgroup(n):this%xgroup(n+1)-1), &
                 value => value(this%xgroup(n):this%xgroup(n+1)-1))
         do j = 1, size(index)
+          args(-1) = u(index(j))
           associate (fnode => this%mesh%face_node_list_view(index(j)))
             args(1:) = sum(this%mesh%x(:,fnode),dim=2) / size(fnode)
+            args0(1:) = args(1:)
           end associate
           c = this%mesh%area(index(j)) * this%f(n)%eval(args)
-          value(j) = c * (u(index(j)) - this%g(n)%eval(args))
+          value(j) = c * (u(index(j)) - this%g(n)%eval(args0))
         end do
       end associate
     end do
@@ -104,21 +113,48 @@ contains
     real(r8), intent(in) :: u(:)
     real(r8), allocatable, intent(out) :: deriv(:)
     integer :: n, j
-    real(r8) :: args(0:size(this%mesh%x,dim=1))
+    real(r8) :: args(-1:size(this%mesh%x,dim=1))
+    real(r8) :: args0(0:size(this%mesh%x,dim=1))
+    real(r8) :: area, dh, h, tamb
     ASSERT(allocated(this%index))
     allocate(deriv(size(this%index)))
     args(0) = t
+    args0(0) = t
     do n = 1, this%ngroup
       associate(index => this%index(this%xgroup(n):this%xgroup(n+1)-1), &
                 deriv => deriv(this%xgroup(n):this%xgroup(n+1)-1))
         do j = 1, size(index)
+          args(-1) = u(index(j))
           associate (fnode => this%mesh%face_node_list_view(index(j)))
             args(1:) = sum(this%mesh%x(:,fnode),dim=2) / size(fnode)
+            args0(1:) = args(1:)
           end associate
-          deriv(j) = this%mesh%area(index(j)) * this%f(n)%eval(args)
+          area = this%mesh%area(index(j))
+          h = this%f(n)%eval(args)
+          tamb = this%g(n)%eval(args0)
+          call compute_first_arg_deriv(this%f(n), args, dh)
+          deriv(j) = area * (h + dh * (u(index(j)) - tamb))
         end do
       end associate
     end do
   end subroutine compute_deriv
+
+  subroutine compute_first_arg_deriv(f, args, deriv)
+    type(scalar_func_box), intent(in) :: f
+    real(r8), intent(inout) :: args(:)
+    real(r8), intent(out) :: deriv
+    real(r8) :: fdinc, fm, fp, u
+
+    ! Differentiate with respect to the first function argument.
+    u = args(1)
+    fdinc = max(1.0_r8, abs(u)) * sqrt(epsilon(1.0_r8))
+    fdinc = scale(1.0_r8, exponent(fdinc))
+    args(1) = u + fdinc
+    fp = f%eval(args)
+    args(1) = u - fdinc
+    fm = f%eval(args)
+    args(1) = u
+    deriv = (fp - fm) / (2*fdinc)
+  end subroutine compute_first_arg_deriv
 
 end module htc_bndry_func_type
