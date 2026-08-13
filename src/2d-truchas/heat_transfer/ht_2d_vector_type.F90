8c6a6bec6029abd3279ce167649afe443e23b906
Neil N. Carlson <neil.n.carlson@gmail.com>
2026-08-13 08:03:53 -0600

    Refine 2D MFD discretization

--- src/2d-truchas/heat_transfer/ht_2d_vector_type.F90
+++ src/2d-truchas/heat_transfer/ht_2d_vector_type.F90
@@ -1,12 +1,25 @@
+!!
 !! HT_2D_VECTOR_TYPE
 !!
-!! State vector for the 2D heat-transfer DAE.
+!! This module defines the concrete vector type used by the 2D heat-transfer
+!! solver. It stores the cell enthalpy, cell temperature, and face temperature
+!! unknowns using wide mesh-sized arrays whose trailing off-process entries
+!! provide persistent workspace for model assembly routines.
 !!
-!! The cell enthalpy, cell temperature, and face temperature components are
-!! stored separately.  Each mesh-associated component includes its
-!! off-process entries as persistent workspace.  Abstract VECTOR operations
-!! act only on the on-process entries; model code gathers off-process values
-!! explicitly before using them.
+!! Neil Carlson <neil.n.carlson@gmail.com>, August 2026
+!! SPDX-License-Identifier: BSD-3-Clause
+!!
+!! Notes
+!!
+!! The abstract vector operations act only on the on-process portions of the
+!! cell and face arrays. Off-process entries are scratch values and must be
+!! gathered explicitly by model code before they are used.
+!!
+!! Vector arguments use intent(inout) even when the operation is conceptually
+!! data-only output. The vector object carries structural metadata, such as
+!! mesh and component layout, that remains input to the operation while the
+!! numeric component data may be overwritten.
+!!
 
 module ht_2d_vector_type
 
@@ -40,55 +53,45 @@ module ht_2d_vector_type
     generic :: init => init_mesh, init_mold
     procedure, private :: init_mesh, init_mold
     procedure :: gather_offp
-  end type ht_2d_vector
+  end type
 
 contains
 
   subroutine init_mesh(this, mesh)
     class(ht_2d_vector), intent(out) :: this
     type(unstr_2d_mesh), intent(in), target :: mesh
-
     this%mesh => mesh
     allocate(this%hc(mesh%ncell), this%tc(mesh%ncell), this%tf(mesh%nface))
-  end subroutine init_mesh
-
+  end subroutine
 
   subroutine init_mold(this, mold)
     class(ht_2d_vector), intent(out) :: this
     class(ht_2d_vector), intent(in) :: mold
-
     call this%init(mold%mesh)
-  end subroutine init_mold
-
+  end subroutine
 
   subroutine gather_offp(this)
     class(ht_2d_vector), intent(inout) :: this
-
     call this%mesh%cell_imap%gather_offp(this%hc)
     call this%mesh%cell_imap%gather_offp(this%tc)
     call this%mesh%face_imap%gather_offp(this%tf)
-  end subroutine gather_offp
-
+  end subroutine
 
   subroutine clone1(this, clone)
     class(ht_2d_vector), intent(in) :: this
     class(vector), allocatable, intent(out) :: clone
-
     allocate(ht_2d_vector :: clone)
     select type (clone)
     class is (ht_2d_vector)
       call clone%init(this)
     end select
-  end subroutine clone1
-
+  end subroutine
 
   subroutine clone2(this, clone, n)
     class(ht_2d_vector), intent(in) :: this
     class(vector), allocatable, intent(out) :: clone(:)
     integer, intent(in) :: n
-
     integer :: j
-
     allocate(ht_2d_vector :: clone(n))
     select type (clone)
     class is (ht_2d_vector)
@@ -96,13 +99,11 @@ contains
         call clone(j)%init(this)
       end do
     end select
-  end subroutine clone2
-
+  end subroutine
 
   subroutine copy_(dest, src)
     class(ht_2d_vector), intent(inout) :: dest
     class(vector), intent(in) :: src
-
     select type (src)
     class is (ht_2d_vector)
       associate (ncell_onP => dest%mesh%ncell_onP, nface_onP => dest%mesh%nface_onP)
@@ -111,38 +112,32 @@ contains
         dest%tf(:nface_onP) = src%tf(:nface_onP)
       end associate
     end select
-  end subroutine copy_
-
+  end subroutine
 
   subroutine setval(this, val)
     class(ht_2d_vector), intent(inout) :: this
     real(r8), intent(in) :: val
-
     associate (ncell_onP => this%mesh%ncell_onP, nface_onP => this%mesh%nface_onP)
       this%hc(:ncell_onP) = val
       this%tc(:ncell_onP) = val
       this%tf(:nface_onP) = val
     end associate
-  end subroutine setval
-
+  end subroutine
 
   subroutine scale(this, a)
     class(ht_2d_vector), intent(inout) :: this
     real(r8), intent(in) :: a
-
     associate (ncell_onP => this%mesh%ncell_onP, nface_onP => this%mesh%nface_onP)
       this%hc(:ncell_onP) = a*this%hc(:ncell_onP)
       this%tc(:ncell_onP) = a*this%tc(:ncell_onP)
       this%tf(:nface_onP) = a*this%tf(:nface_onP)
     end associate
-  end subroutine scale
-
+  end subroutine
 
   subroutine update1_(this, a, x)
     class(ht_2d_vector), intent(inout) :: this
     class(vector), intent(in) :: x
     real(r8), intent(in) :: a
-
     select type (x)
     class is (ht_2d_vector)
       associate (ncell_onP => this%mesh%ncell_onP, nface_onP => this%mesh%nface_onP)
@@ -151,14 +146,12 @@ contains
         this%tf(:nface_onP) = a*x%tf(:nface_onP) + this%tf(:nface_onP)
       end associate
     end select
-  end subroutine update1_
-
+  end subroutine
 
   subroutine update2_(this, a, x, b)
     class(ht_2d_vector), intent(inout) :: this
     class(vector), intent(in) :: x
     real(r8), intent(in) :: a, b
-
     select type (x)
     class is (ht_2d_vector)
       associate (ncell_onP => this%mesh%ncell_onP, nface_onP => this%mesh%nface_onP)
@@ -167,14 +160,12 @@ contains
         this%tf(:nface_onP) = a*x%tf(:nface_onP) + b*this%tf(:nface_onP)
       end associate
     end select
-  end subroutine update2_
-
+  end subroutine
 
   subroutine update3_(this, a, x, b, y)
     class(ht_2d_vector), intent(inout) :: this
     class(vector), intent(in) :: x, y
     real(r8), intent(in) :: a, b
-
     select type (x)
     class is (ht_2d_vector)
       select type (y)
@@ -186,14 +177,12 @@ contains
         end associate
       end select
     end select
-  end subroutine update3_
-
+  end subroutine
 
   subroutine update4_(this, a, x, b, y, c)
     class(ht_2d_vector), intent(inout) :: this
     class(vector), intent(in) :: x, y
     real(r8), intent(in) :: a, b, c
-
     select type (x)
     class is (ht_2d_vector)
       select type (y)
@@ -205,14 +194,12 @@ contains
         end associate
       end select
     end select
-  end subroutine update4_
-
+  end subroutine
 
   function dot_(x, y) result(dp)
     class(ht_2d_vector), intent(in) :: x
     class(vector), intent(in) :: y
     real(r8) :: dp
-
     select type (y)
     class is (ht_2d_vector)
       dp = dot_product(x%hc(:x%mesh%ncell_onP), y%hc(:x%mesh%ncell_onP)) &
@@ -220,46 +207,36 @@ contains
          + dot_product(x%tf(:x%mesh%nface_onP), y%tf(:x%mesh%nface_onP))
       dp = global_sum(dp)
     end select
-  end function dot_
-
+  end function
 
   real(r8) function norm1_(this) result(norm)
     class(ht_2d_vector), intent(in) :: this
-
     norm = sum(abs(this%hc(:this%mesh%ncell_onP))) + sum(abs(this%tc(:this%mesh%ncell_onP))) &
          + sum(abs(this%tf(:this%mesh%nface_onP)))
     norm = global_sum(norm)
-  end function norm1_
-
+  end function
 
   real(r8) function norm2_(this) result(norm)
     class(ht_2d_vector), intent(in) :: this
-
     norm = norm2(this%hc(:this%mesh%ncell_onP))**2 + norm2(this%tc(:this%mesh%ncell_onP))**2 &
          + norm2(this%tf(:this%mesh%nface_onP))**2
     norm = sqrt(global_sum(norm))
-  end function norm2_
-
+  end function
 
   real(r8) function norm_max_(this) result(norm)
     class(ht_2d_vector), intent(in) :: this
-
     norm = max(0.0_r8, maxval(abs(this%hc(:this%mesh%ncell_onP))), &
                maxval(abs(this%tc(:this%mesh%ncell_onP))), maxval(abs(this%tf(:this%mesh%nface_onP))))
     norm = global_maxval(norm)
-  end function norm_max_
-
+  end function
 
   function checksum(this, full) result(string)
     use md5_hash_type
-
     class(ht_2d_vector), intent(in) :: this
     logical, intent(in), optional :: full
     character(:), allocatable :: string
-
     type(md5_hash) :: hash
     logical :: strict
-
     strict = .true.
     if (present(full)) strict = .not.full
     if (strict) then
@@ -272,6 +249,6 @@ contains
       call hash%update(this%tf)
     end if
     string = hash%hexdigest()
-  end function checksum
+  end function
 
 end module ht_2d_vector_type
--- src/2d-truchas/heat_transfer/mfd_2d_disc_type.F90
+++ src/2d-truchas/heat_transfer/mfd_2d_disc_type.F90
@@ -1,15 +1,31 @@
-! TODO: finish documentation
 !!
 !! MFD_2D_DISC_TYPE
 !!
-!! David Neill-Asanza <dhna@lanl.gov>
-!! January 2020
+!! This module defines the MFD_2D_DISC type used by thermal diffusion models to
+!! represent a mimetic finite difference (MFD) discretization on an
+!! unstructured mesh. Its primary state is the cell-local inverse flux mass
+!! matrix MINV, stored in upper packed format. The standard construction
+!! follows the generic polyhedral-cell MFD method described in:
 !!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!   Konstantin Lipnikov, Gianmarco Manzini, and Mikhail Shashkov. Mimetic
+!!   finite difference method. Journal of Computational Physics, 257:1163-1227,
+!!   2014.
 !!
-!! This file is part of Truchas. 3-Clause BSD license; see the LICENSE file.
+!!   V. Gyrya, K. Lipnikov, G. Manzini, and D. Svyatskiy. M-adaptation in the
+!!   mimetic finite difference method. Mathematical Models & Methods in Applied
+!!   Sciences, 24(8):1621-1663, 2014.
 !!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!! The type is used by trusted code; its public data components should be
+!! treated as read-only after initialization. It treats its mesh as a local
+!! serial mesh, with no ownership or on/off-process semantics. Callers are
+!! responsible for ensuring input values are valid on the full local mesh
+!! extent they pass in, and for interpreting which entries of any result are
+!! meaningful for their distributed algorithm.
+!!
+!! David Neill-Asanza <davidhneill@gmail.com>, January 2020
+!! Neil Carlson <neil.n.carlson@gmail.com>, July 2026
+!! SPDX-License-Identifier: BSD-3-Clause
+!!
 
 #include "f90_assert.fpp"
 
@@ -20,45 +36,45 @@ module mfd_2d_disc_type
   implicit none
   private
 
+  integer, parameter :: MFD_CELL_NFACE_MAX = 4
+
   type, public :: mfd_2d_disc
-    type(unstr_2d_mesh), pointer :: mesh => null()  ! reference only - do not own
+    type(unstr_2d_mesh), pointer :: mesh => null()  ! unowned reference
     integer, allocatable :: xminv(:)
     real(r8), allocatable :: minv(:)
+    integer, private :: nface_max
   contains
-    procedure :: init => mfd_2d_disc_init
-    procedure :: apply_diff => mfd_2d_disc_apply_diff
-  end type mfd_2d_disc
+    procedure :: init
+    procedure :: apply_diff
+    procedure :: add_diff
+  end type
 
-  type, public :: mfd_2d_cell
-    integer :: nface
-    real(r8), allocatable :: cell_center(:)
-    real(r8), allocatable :: face_center(:,:)
-    real(r8), allocatable :: face_normal(:,:)
-    real(r8), allocatable :: face_area(:)
+  !! Private type for internal use.
+  type :: mfd_cell
+    integer :: nfaces = 0
+    real(r8) :: face_centers(2,MFD_CELL_NFACE_MAX)
+    real(r8) :: cell_center(2)
+    real(r8) :: face_area(MFD_CELL_NFACE_MAX)
+    real(r8) :: face_normals(2,MFD_CELL_NFACE_MAX)
     real(r8) :: volume
   contains
-    procedure :: init => mfd_2d_cell_init
-    procedure :: compute_flux_matrix_inv => mfd_2d_cell_compute_flux_matrix_inv
-  end type mfd_2d_cell
+    procedure :: init => mfd_cell_init
+    procedure :: compute_flux_matrix_inv
+  end type
 
 contains
 
-  !TODO: add optional MINV initialization? See 3D mfd_disc.
-  subroutine mfd_2d_disc_init(this, mesh)
+  subroutine init(this, mesh)
+
     class(mfd_2d_disc), intent(out) :: this
     type(unstr_2d_mesh), intent(in), target :: mesh
-    this%mesh => mesh
-    call init_minv(this)
-  end subroutine mfd_2d_disc_init
-
-
-  !! Allocates and initializes the THIS%MINV.
-  subroutine init_minv(this)
-
-    type(mfd_2d_disc), intent(inout) :: this
 
     integer :: j, n
-    type(mfd_2d_cell), allocatable :: cell
+    type(mfd_cell) :: cell
+
+    this%mesh => mesh
+    this%nface_max = max_cell_faces(mesh)
+    ASSERT(this%nface_max <= MFD_CELL_NFACE_MAX)
 
     !! Initialize XMINV indexing array.
     allocate(this%xminv(this%mesh%ncell+1))
@@ -68,32 +84,45 @@ contains
       this%xminv(j+1) = this%xminv(j) + (n*(n+1))/2
     end do
 
-    !! Populate MINV, where MINV(XMINV(j):XMINV(j+1)-1) is the inverse M matrix
-    !! of cell j, stored in upper packed matrix format.
+    !! Populate MINV, where MINV(XMINV(j):XMINV(j+1)-1) is the inverse of
+    !! the mass matrix M of cell j, stored in upper packed matrix format.
     allocate(this%minv(this%xminv(this%mesh%ncell+1)-1))
-    allocate(cell)
     do j = 1, this%mesh%ncell
       associate (minv => this%minv(this%xminv(j):this%xminv(j+1)-1))
-          call cell%init(j, this%mesh)
-          call cell%compute_flux_matrix_inv(1.0_r8, minv)
+        call cell%init(j, this%mesh)
+        call cell%compute_flux_matrix_inv(1.0_r8, minv)
       end associate
     end do
 
-  end subroutine init_minv
+  contains
 
+    integer function max_cell_faces(mesh)
+      type(unstr_2d_mesh), intent(in) :: mesh
+      if (mesh%ncell == 0) then
+        max_cell_faces = 0
+      else
+        max_cell_faces = maxval(mesh%cstart(2:) - mesh%cstart(:mesh%ncell))
+      end if
+    end function
 
-  !! Applies local MFD diffusion operator
-  subroutine mfd_2d_disc_apply_diff (this, coef, ucell, uface, rcell, rface)
+  end subroutine init
 
-    use upper_packed_matrix_procs, only: sym_matmul
+  !! Apply the local MFD diffusion operator as a serial operator over the full
+  !! mesh held by this object. No communication is performed; COEF, UCELL, and
+  !! UFACE must be valid over their full local extents. RCELL and RFACE are
+  !! filled over those same extents, and callers decide which entries to use.
+
+  subroutine apply_diff(this, coef, ucell, uface, rcell, rface)
+
+    !use upper_packed_matrix_procs, only: upm_sym_matvec
 
     class(mfd_2d_disc), intent(in) :: this
     real(r8), intent(in)  :: coef(:)
     real(r8), intent(in)  :: ucell(:), uface(:)
     real(r8), intent(out) :: rcell(:), rface(:)
 
-    integer :: j
-    real(r8), allocatable :: flux(:)
+    integer :: j, n
+    real(r8), allocatable :: du(:), flux(:)
 
     ASSERT(size(coef) == this%mesh%ncell)
     ASSERT(size(ucell) == size(coef))
@@ -101,27 +130,89 @@ contains
     ASSERT(size(uface) == this%mesh%nface)
     ASSERT(size(rface) == size(uface))
 
+    allocate(du(this%nface_max), flux(this%nface_max))
     rface = 0.0_r8
     do j = 1, this%mesh%ncell
       associate (cface => this%mesh%cface(this%mesh%cstart(j):this%mesh%cstart(j+1)-1), &
                  minv  => this%minv(this%xminv(j):this%xminv(j+1)-1))
-        flux = coef(j) * sym_matmul(minv, ucell(j) - uface(cface))
-        rface(cface) = rface(cface) - flux
-        rcell(j) = sum(flux)
+        n = size(cface)
+        du(:n) = ucell(j) - uface(cface)
+        call upm_sym_matvec(minv, du(:n), flux(:n))
+        flux(:n) = coef(j) * flux(:n)
+        rface(cface) = rface(cface) - flux(:n)
+        rcell(j) = sum(flux(:n))
       end associate
     end do
 
-  end subroutine mfd_2d_disc_apply_diff
+  end subroutine apply_diff
 
+  !! Add the local MFD diffusion operator result into RCELL and RFACE. This
+  !! has the same input contract as APPLY_DIFF, but accumulates into the output
+  !! arrays instead of overwriting them.
 
-  !! MFD_2D_CELL type bound procedures !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  subroutine add_diff(this, coef, ucell, uface, rcell, rface)
 
-  subroutine mfd_2d_cell_init(this, cellid, mesh)
+    !use upper_packed_matrix_procs, only: upm_sym_matvec
+
+    class(mfd_2d_disc), intent(in) :: this
+    real(r8), intent(in)    :: coef(:)
+    real(r8), intent(in)    :: ucell(:), uface(:)
+    real(r8), intent(inout) :: rcell(:), rface(:)
+
+    integer :: j, n
+    real(r8), allocatable :: du(:), flux(:)
+
+    ASSERT(size(coef) == this%mesh%ncell)
+    ASSERT(size(ucell) == size(coef))
+    ASSERT(size(rcell) == size(ucell))
+    ASSERT(size(uface) == this%mesh%nface)
+    ASSERT(size(rface) == size(uface))
+
+    allocate(du(this%nface_max), flux(this%nface_max))
+    do j = 1, this%mesh%ncell
+      associate (cface => this%mesh%cface(this%mesh%cstart(j):this%mesh%cstart(j+1)-1), &
+                 minv  => this%minv(this%xminv(j):this%xminv(j+1)-1))
+        n = size(cface)
+        du(:n) = ucell(j) - uface(cface)
+        call upm_sym_matvec(minv, du(:n), flux(:n))
+        flux(:n) = coef(j) * flux(:n)
+        rface(cface) = rface(cface) - flux(:n)
+        rcell(j) = rcell(j) + sum(flux(:n))
+      end associate
+    end do
+
+  end subroutine add_diff
+
+  !! Multiply an upper-packed symmetric matrix by a vector.
+  !! NB: Provided by a newer version of the UPPER_PACKED_MATRIX_PROCS module.
+
+  pure subroutine upm_sym_matvec(a, b, c)
+    real(r8), intent(in) :: a(:), b(:)
+    real(r8), intent(out) :: c(:)
+    integer :: i, j, l
+    real(r8) :: bi, ci
+    l = 1
+    do i = 1, size(b)
+      bi = b(i)
+      ci = 0.0_r8
+      do j = 1, i - 1
+        ci = ci + a(l) * b(j)
+        c(j) = c(j) + a(l) * bi
+        l = l + 1
+      end do
+      c(i) = ci + a(l) * bi
+      l = l + 1
+    end do
+  end subroutine
+
+  !! MFD_CELL type bound procedures !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  subroutine mfd_cell_init(this, cellid, mesh)
 
     use cell_geometry, only: cell_centroid_2d
     use bitfield_type, only: btest
 
-    class(mfd_2d_cell), intent(out) :: this
+    class(mfd_cell), intent(out) :: this
     integer, intent(in) :: cellid
     type(unstr_2d_mesh), intent(in) :: mesh
 
@@ -132,44 +223,38 @@ contains
                cface => mesh%cface(mesh%cstart(cellid):mesh%cstart(cellid+1)-1))
       this%cell_center = cell_centroid_2d(mesh%x(:,cnode))
       this%volume      = mesh%volume(cellid)
-      this%nface       = size(cface)
-      this%face_area   = mesh%area(cface)
-      allocate(this%face_normal(2,this%nface), this%face_center(2,this%nface))
-      do j = 1, this%nface
-        this%face_center(:,j) = 0.5_r8 * sum(mesh%x(:,mesh%fnode(:,cface(j))), dim=2)
+      this%nfaces      = size(cface)
+      this%face_area(:this%nfaces) = mesh%area(cface)
+      do j = 1, this%nfaces
+        this%face_centers(:,j) = 0.5_r8 * sum(mesh%x(:,mesh%fnode(:,cface(j))), dim=2)
         !! Outward facing normals
         parity = merge(-1.0, 1.0, btest(mesh%cfpar(cellid),j))
-        this%face_normal(:,j) = mesh%normal(:,cface(j)) * parity
+        this%face_normals(:,j) = mesh%normal(:,cface(j)) * parity
       end do
     end associate
 
-  end subroutine mfd_2d_cell_init
+  end subroutine mfd_cell_init
 
-  !! This is procedure computes inverse of the MFD mass matrix for a polygonal
-  !! cell using the standard choice of MFD parameters:
-  !! W - is an inverse of mass matrix
-  !!
-  !! W = (coef/volume)*N*N' + (coef/volume)*(I - Q*Q')
-  !!
-  !! N - matrix of normals
-  !! Q - orthonormal basis of range(R)
-  !! R - rows of (face center)-(cell center) for each cell face
+  !! Compute the inverse MFD mass matrix for a polyhedral cell using the
+  !! standard parameter choice; see references listed above. With N the matrix
+  !! of face normals and Q an orthonormal basis for range(R), the inverse is
+  !! W = (coef/volume) * (N*N' + I - Q*Q').
 
-  subroutine mfd_2d_cell_compute_flux_matrix_inv(this, coef, matrix)
+  subroutine compute_flux_matrix_inv(this, coef, matrix)
 
-    class(mfd_2d_cell), intent(in) :: this
+    class(mfd_cell), intent(in) :: this
     real(r8), intent(in) :: coef
     real(r8), intent(out) :: matrix(:)
 
     integer :: i, j, n, loc
     real(r8) :: stab_val
-    real(r8) :: Q(2,this%nface), R(2,this%nface)
+    real(r8) :: Q(2,this%nfaces), R(2,this%nfaces)
 
     matrix = 0.0_r8
-    n = this%nface
+    n = this%nfaces
 
     do i = 1, n
-      R(:,i) = (this%face_center(:,i) - this%cell_center(:))*this%face_area(i)
+      R(:,i) = (this%face_centers(:,i) - this%cell_center(:))*this%face_area(i)
     end do
 
     !! Compute Q by modified Grammm-Schimdt orthogonalization
@@ -182,7 +267,7 @@ contains
        do j = i, n
           loc = i + j*(j-1)/2
 
-          matrix(loc) = dot_product(this%face_normal(:,i), this%face_normal(:,j))
+          matrix(loc) = dot_product(this%face_normals(:,i), this%face_normals(:,j))
 
           stab_val = -dot_product(Q(:,i), Q(:,j))
           if (i.eq.j) stab_val = stab_val + 1
@@ -193,6 +278,6 @@ contains
        end do
     end do
 
-  end subroutine mfd_2d_cell_compute_flux_matrix_inv
+  end subroutine compute_flux_matrix_inv
 
 end module mfd_2d_disc_type
