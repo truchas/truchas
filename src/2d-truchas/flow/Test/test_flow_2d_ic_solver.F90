4859a989fd6426d913c52c020561cb1a371d88ac
Neil N. Carlson <neil.n.carlson@gmail.com>
2026-08-18 16:34:19 -0600

    Test repair of incompatible 2D flow initial states

--- src/2d-truchas/flow/Test/test_flow_2d_ic_solver.F90
+++ src/2d-truchas/flow/Test/test_flow_2d_ic_solver.F90
@@ -24,6 +24,7 @@ program test_flow_2d_ic_solver
 
   status = 0
   call test_uniform_velocity
+  call test_incompatible_velocity
 
   call halt_parallel_communication
   stop status
@@ -62,6 +63,48 @@ contains
     call require(maxval(abs(flux)) < 1.0e-8_r8, 'initial face velocity is not solenoidal')
     call require(maxval(abs(state%vel_cc(:,1:mesh%ncell_onP) - velocity(:,1:mesh%ncell_onP))) < 1.0e-8_r8, &
         'compatible uniform initial velocity was changed')
+  end subroutine
+
+
+  subroutine test_incompatible_velocity
+    type(unstr_2d_mesh), pointer :: mesh
+    type(flow_2d_model), target :: model
+    type(flow_2d_state), target :: state
+    type(flow_2d_solver) :: solver
+    type(parameter_list), target :: bc_params, momentum_params, projection_params
+    type(parameter_list), pointer :: plist
+    real(r8), allocatable :: velocity(:,:), flux(:)
+    real(r8) :: boundary_velocity
+    character(:), allocatable :: errmsg
+    integer :: stat, f
+
+    mesh => new_unstr_2d_mesh([0.0_r8, 0.0_r8], [1.0_r8, 1.0_r8], [8, 8], 0.0_r8, 0.0_r8)
+    plist => bc_params%sublist('wall')
+    call plist%set('type', 'no-slip')
+    call plist%set('face-set-ids', [1,2,3,4])
+    call model%init(mesh, bc_params, 1.0_r8, 1.0_r8, stat, errmsg)
+    call require(stat == 0, 'incompatible-velocity model initialization failed')
+    if (stat /= 0) return
+    call state%init(mesh)
+    call set_solver_params(momentum_params)
+    call set_solver_params(projection_params)
+    call solver%init(model, state, momentum_params, projection_params)
+    allocate(velocity(2,mesh%ncell), flux(mesh%ncell_onP))
+    velocity = spread([1.0_r8, -0.5_r8], dim=2, ncopies=mesh%ncell)
+    call solver%set_initial_state(0.0_r8, 1.0_r8, velocity, stat)
+    call require(stat == 0, 'incompatible initial-condition solve did not converge')
+    if (stat /= 0) return
+    call model%operators%divergence(state%vel_fn, flux)
+    call require(maxval(abs(flux)) < 1.0e-8_r8, &
+        'repaired initial face velocity is not solenoidal')
+    boundary_velocity = 0.0_r8
+    do f = 1, mesh%nface_onP
+      if (mesh%fcell(2,f) == 0) boundary_velocity = max(boundary_velocity, abs(state%vel_fn(f)))
+    end do
+    call require(boundary_velocity < 1.0e-8_r8, &
+        'repaired initial velocity does not satisfy no-slip boundaries')
+    call require(maxval(abs(state%vel_cc(:,1:mesh%ncell_onP) - velocity(:,1:mesh%ncell_onP))) > 1.0e-6_r8, &
+        'incompatible initial velocity was not repaired')
   end subroutine
 
 
