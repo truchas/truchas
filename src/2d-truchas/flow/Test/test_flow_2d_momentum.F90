54b0352b425eaed07df3c70087c37ba48eaf8c77
Neil N. Carlson <neil.n.carlson@gmail.com>
2026-08-18 11:42:16 -0600

    Add 2D flow block momentum operator

---
+++ src/2d-truchas/flow/Test/test_flow_2d_momentum.F90
@@ -0,0 +1,113 @@
+program test_flow_2d_momentum
+
+  use,intrinsic :: iso_fortran_env, only: r8 => real64
+  use parallel_communication
+  use truchas_env, only: prefix, overwrite_output
+  use truchas_logging_services
+  use parameter_list_type
+  use unstr_2d_mesh_type
+  use unstr_2d_mesh_factory
+  use flow_2d_operators_type
+  use flow_2d_bc_type
+  use flow_2d_momentum_type
+  use pbsr_matrix_type
+  implicit none
+
+  integer :: status
+
+  call init_parallel_communication
+  prefix = 'run'
+  overwrite_output = .true.
+  call TLS_initialize
+  call TLS_set_verbosity(TLS_VERB_NORMAL)
+
+  status = 0
+  call test_momentum
+
+  call halt_parallel_communication
+  stop status
+
+contains
+
+  subroutine test_momentum
+    type(unstr_2d_mesh), pointer :: mesh
+    type(flow_2d_operators), target :: operators
+    type(flow_2d_momentum), target :: momentum
+    type(flow_2d_bc) :: bc
+    type(parameter_list), target :: velocity_params, slip_params
+    type(parameter_list), pointer :: plist
+    type(pbsr_matrix), pointer :: matrix
+    real(r8), allocatable :: density(:), viscosity(:), velocity(:,:), rhs(:,:), result(:,:)
+    character(:), allocatable :: errmsg
+    integer :: stat, f, c, entry
+    logical :: dirichlet_solution, coupled_slip
+
+    mesh => new_unstr_2d_mesh([0.0_r8, 0.0_r8], [1.0_r8, 1.0_r8], [8, 8], 0.0_r8, 0.0_r8)
+    call rotate_mesh(mesh, 30.0_r8)
+    call operators%init(mesh)
+    call momentum%init(mesh, operators)
+    allocate(density(mesh%ncell), viscosity(mesh%nface), velocity(2,mesh%ncell), &
+        rhs(2,mesh%ncell_onP), result(2,mesh%ncell_onP))
+    density = 0.0_r8
+    viscosity = 1.0_r8
+    velocity = spread([1.5_r8, -0.75_r8], dim=2, ncopies=mesh%ncell)
+
+    plist => velocity_params%sublist('wall')
+    call plist%set('type', 'velocity')
+    call plist%set('face-set-ids', [1])
+    call plist%set('velocity', [1.5_r8, -0.75_r8])
+    call bc%init(mesh, velocity_params, stat, errmsg)
+    call require(stat == 0, 'velocity boundary condition initialization failed')
+    call bc%compute(0.0_r8)
+    call momentum%assemble(1.0_r8, density, viscosity, bc, rhs)
+    matrix => momentum%matrix()
+    call matrix%matvec(velocity, result)
+    dirichlet_solution = maxval(abs(result - rhs)) < 1.0e-12_r8
+    call require(dirichlet_solution, 'constant velocity Dirichlet solution does not satisfy momentum system')
+
+    plist => slip_params%sublist('wall')
+    call plist%set('type', 'free-slip')
+    call plist%set('face-set-ids', [1])
+    call bc%init(mesh, slip_params, stat, errmsg)
+    call require(stat == 0, 'free-slip boundary condition initialization failed')
+    call bc%compute(0.0_r8)
+    call momentum%assemble(1.0_r8, density, viscosity, bc, rhs)
+    matrix => momentum%matrix()
+    coupled_slip = .false.
+    do f = 1, mesh%nface_onP
+      if (mesh%fcell(2,f) /= 0) cycle
+      if (abs(mesh%unit_normal(1,f)*mesh%unit_normal(2,f)) < 1.0e-12_r8) cycle
+      c = mesh%fcell(1,f)
+      entry = matrix%graph%index(c, c)
+      coupled_slip = abs(matrix%values(1,2,entry)) > 1.0e-12_r8 .and. &
+          abs(matrix%values(1,2,entry) - matrix%values(2,1,entry)) < 1.0e-12_r8
+      if (coupled_slip) exit
+    end do
+    call require(global_any(coupled_slip), 'free-slip condition did not add a symmetric velocity block coupling')
+  end subroutine
+
+
+  subroutine rotate_mesh(mesh, angle)
+    type(unstr_2d_mesh), intent(inout) :: mesh
+    real(r8), intent(in) :: angle
+
+    real(r8) :: theta, rotation(2,2)
+
+    theta = angle*acos(-1.0_r8)/180.0_r8
+    rotation = reshape([cos(theta), sin(theta), -sin(theta), cos(theta)], [2,2])
+    mesh%x = matmul(rotation, mesh%x)
+    call mesh%compute_geometry()
+  end subroutine
+
+
+  subroutine require(condition, message)
+    logical, intent(in) :: condition
+    character(*), intent(in) :: message
+
+    if (global_any(.not.condition)) then
+      if (is_IOP) print '("ERROR: ",a)', message
+      status = 1
+    end if
+  end subroutine
+
+end program test_flow_2d_momentum
---
+++ src/2d-truchas/flow/flow_2d_momentum_type.F90
@@ -0,0 +1,136 @@
+!!
+!! FLOW_2D_MOMENTUM_TYPE
+!!
+!! This module defines FLOW_2D_MOMENTUM, the block-structured finite-volume
+!! operator for the cell-centered velocity predictor in two-dimensional
+!! incompressible flow. The matrix has one 2-by-2 block per cell pair. Its
+!! first implementation contains inertial and first-order viscous terms.
+!!
+!! Velocity Dirichlet and free-slip conditions are eliminated through the
+!! boundary face fluxes. Free slip contributes a normal outer-product block,
+!! which couples the two Cartesian velocity components.
+!!
+!! Neil Carlson <neil.n.carlson@gmail.com>, August 2026
+!! SPDX-License-Identifier: BSD-3-Clause
+!!
+
+#include "f90_assert.fpp"
+
+module flow_2d_momentum_type
+
+  use,intrinsic :: iso_fortran_env, only: r8 => real64
+  use unstr_2d_mesh_type
+  use flow_2d_operators_type
+  use flow_2d_bc_type
+  use pbsr_matrix_type
+  implicit none
+  private
+
+  type, public :: flow_2d_momentum
+    private
+    type(unstr_2d_mesh), pointer :: mesh => null()  ! unowned reference
+    type(flow_2d_operators), pointer :: operators => null()  ! unowned reference
+    type(pbsr_matrix) :: matrix_
+  contains
+    procedure :: init
+    procedure :: assemble
+    procedure :: matrix
+  end type
+
+contains
+
+  subroutine init(this, mesh, operators)
+    class(flow_2d_momentum), intent(out) :: this
+    type(unstr_2d_mesh), target, intent(in) :: mesh
+    type(flow_2d_operators), target, intent(in) :: operators
+
+    type(pcsr_graph), pointer :: graph
+    integer :: c, i, neighbor
+
+    this%mesh => mesh
+    this%operators => operators
+    allocate(graph)
+    call graph%init(mesh%cell_imap)
+    do c = 1, mesh%ncell_onP
+      call graph%add_edge(c, c)
+      do i = mesh%cstart(c), mesh%cstart(c+1)-1
+        neighbor = mesh%cnhbr(i)
+        if (neighbor > 0) call graph%add_edge(c, neighbor)
+      end do
+    end do
+    call graph%add_complete()
+    call this%matrix_%init(2, graph, take_graph=.true.)
+  end subroutine
+
+
+  !! Assemble rho*volume*I - dt*div(mu grad(u)) and its velocity-Dirichlet
+  !! contribution to RHS. BC must already have been evaluated at the required
+  !! time.
+  subroutine assemble(this, dt, density_c, viscosity_f, bc, rhs)
+    class(flow_2d_momentum), intent(inout) :: this
+    real(r8), intent(in) :: dt, density_c(:), viscosity_f(:)
+    type(flow_2d_bc), intent(in) :: bc
+    real(r8), intent(out) :: rhs(:,:)
+
+    integer :: c, i, f, neighbor, n
+    real(r8) :: coefficient, identity(2,2), normal(2), block(2,2)
+
+    ASSERT(dt >= 0.0_r8)
+    ASSERT(size(density_c) >= this%mesh%ncell)
+    ASSERT(size(viscosity_f) >= this%mesh%nface)
+    ASSERT(size(rhs,1) == 2 .and. size(rhs,2) == this%mesh%ncell_onP)
+
+    identity = 0.0_r8
+    identity(1,1) = 1.0_r8
+    identity(2,2) = 1.0_r8
+    call this%matrix_%set_all(0.0_r8)
+    rhs = 0.0_r8
+
+    do c = 1, this%mesh%ncell_onP
+      call this%matrix_%add_to(c, c, density_c(c)*this%mesh%volume(c)*identity)
+      do i = this%mesh%cstart(c), this%mesh%cstart(c+1)-1
+        f = this%mesh%cface(i)
+        neighbor = this%mesh%cnhbr(i)
+        if (neighbor == 0) cycle
+        coefficient = dt*viscosity_f(f)*this%mesh%area(f)/this%operators%normal_distance(f)
+        call this%matrix_%add_to(c, c, coefficient*identity)
+        call this%matrix_%add_to(c, neighbor, -coefficient*identity)
+      end do
+    end do
+
+    if (allocated(bc%velocity_dirichlet)) then
+      do n = 1, size(bc%velocity_dirichlet%index)
+        f = bc%velocity_dirichlet%index(n)
+        if (f > this%mesh%nface_onP) cycle
+        c = this%mesh%fcell(1,f)
+        ASSERT(this%mesh%fcell(2,f) == 0)
+        coefficient = dt*viscosity_f(f)*this%mesh%area(f)/this%operators%normal_distance(f)
+        call this%matrix_%add_to(c, c, coefficient*identity)
+        rhs(:,c) = rhs(:,c) + coefficient*bc%velocity_dirichlet%value(:,n)
+      end do
+    end if
+
+    if (allocated(bc%velocity_zero_normal)) then
+      do n = 1, size(bc%velocity_zero_normal%index)
+        f = bc%velocity_zero_normal%index(n)
+        if (f > this%mesh%nface_onP) cycle
+        c = this%mesh%fcell(1,f)
+        ASSERT(this%mesh%fcell(2,f) == 0)
+        coefficient = dt*viscosity_f(f)*this%mesh%area(f)/this%operators%normal_distance(f)
+        normal = this%mesh%unit_normal(:,f)
+        block = coefficient*reshape([normal(1)**2, normal(1)*normal(2), &
+            normal(1)*normal(2), normal(2)**2], [2,2])
+        call this%matrix_%add_to(c, c, block)
+      end do
+    end if
+  end subroutine
+
+
+  function matrix(this)
+    class(flow_2d_momentum), intent(in), target :: this
+    type(pbsr_matrix), pointer :: matrix
+
+    matrix => this%matrix_
+  end function
+
+end module flow_2d_momentum_type
