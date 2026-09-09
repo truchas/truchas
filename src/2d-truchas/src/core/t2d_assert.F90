97350695e9adba4a9985a50229e9c44ec28f76c4
Neil N. Carlson <neil.n.carlson@gmail.com>
2026-09-09 13:15:26 -0600

    Add assertion messages to 2D macros

--- src/2d-truchas/src/core/t2d_assert.F90
+++ src/2d-truchas/src/core/t2d_assert.F90
@@ -2,8 +2,9 @@
 !!  T2D_ASSERT -- Assertions for the 2D Truchas implementation.
 !!
 !!  This procedure is the companion to the t2d_assert.inc include file,
-!!  which defines the ASSERT and INSIST macros.  A failing assertion reports
-!!  the MPI rank and aborts the MPI job when MPI is active.
+!!  which defines the ASSERT, ASSERT_MSG, INSIST, and INSIST_MSG macros.  A
+!!  failing assertion reports the MPI rank and aborts the MPI job when MPI is
+!!  active.
 !!
 !!  Neil N. Carlson <neil.carlson@gmail.com>, September 2026
 !!  SPDX-License-Identifier: BSD-3-Clause
@@ -13,13 +14,14 @@
 !!  collectively.
 !!
 
-subroutine t2d_assert(file, line)
+subroutine t2d_assert(file, line, msg)
 
   use,intrinsic :: iso_fortran_env, only: error_unit
   use mpi_f08
 
   character(*), intent(in) :: file
   integer, intent(in) :: line
+  character(*), intent(in) :: msg
 
   logical :: mpi_is_initialized, mpi_is_finalized
   integer :: rank
@@ -30,11 +32,20 @@ subroutine t2d_assert(file, line)
 
   if (mpi_is_initialized .and. .not.mpi_is_finalized) then
     call MPI_Comm_rank(MPI_COMM_WORLD, rank)
-    write(error_unit,'(a,i0,3a,i0)') 'T2D assertion failed on rank ', rank, ' at ', trim(file), ':', line
+    if (len_trim(msg) > 0) then
+      write(error_unit,'(a,":",i0,": T2D assertion failed on rank ",i0,": ",a)') &
+        trim(file), line, rank, trim(msg)
+    else
+      write(error_unit,'(a,":",i0,": T2D assertion failed on rank ",i0)') trim(file), line, rank
+    end if
     flush(error_unit)
     call MPI_Abort(MPI_COMM_WORLD, 1)
   else
-    write(error_unit,'(3a,i0)') 'T2D assertion failed at ', trim(file), ':', line
+    if (len_trim(msg) > 0) then
+      write(error_unit,'(a,":",i0,": T2D assertion failed: ",a)') trim(file), line, trim(msg)
+    else
+      write(error_unit,'(a,":",i0,": T2D assertion failed")') trim(file), line
+    end if
     flush(error_unit)
   end if
 
